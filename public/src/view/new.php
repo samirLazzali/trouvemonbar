8debceca8070a94592ba2f7675e9fdc09f670aa8
T. Kowalski <thomaskowalski@outlook.com>
2018-04-20 15:08:12 +0200

    Amélioration du style, surtout sur la page d'inscription.

--- public/src/view/new.php
+++ public/src/view/new.php
@@ -1,31 +1,50 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: yeti
- * Date: 19/04/18
- * Time: 11:03
- */
+    require "../config.php";
+    if (getUserFromCookie() != null) {
+        header("Location: feed.php");
+        die();
+    }
 ?>
 <!DOCTYPE HTML>
 <html>
     <head>
         <title>
-            Création de compte
+            Vitz # Création de compte
         </title>
         <meta charset="UTF-8" />
+        <link rel="stylesheet" type="text/css" href="/src/assets/styles/signup.css" />
         <script src="forms.js"></script>
     </head>
     <body>
-        <h2>Création de compte</h2>
-        <form onSubmit="return validate_input_signup();">
-            <input id="field-username" type="text" name="username" placeholder="Nom d'utilisateur" />
-            <input id="field-password" type="password" name="password" placeholder="Mot de passe" />
-            <input id="field-email"    type="text" name="email" placeholder="Adresse email" />
-            <button type="submit" value="Créer son propre compte">
-                Terminer
-            </button>
-        </form>
-        <p>
+        <h1 class="title">Vitz</h1>
+
+        <h2 class="moto">Les ennemis de mes ennemis sont mes amis.</h2>
+
+        <h2 class="pageTitle">Création de compte</h2>
+
+
+        <div id="info-email-registered" class="error infobox">
+            Une erreur est survenue : cet e-mail est déjà enregistré.
+        </div>
+        <div id="info-username-registered" class="error infobox">
+            Une erreur est survenue : ce nom d'utilisateur est déjà utilisé.
+        </div>
+        <div id="info-password-weak" class="error infobox">
+            Une erreur est survenue : votre mot de passe n'est pas assez complexe.
+        </div>
+
+        <div class="form-wrapper">
+            <form onSubmit="return validate_input_signup();">
+                <input class="field topInput" id="field-username" type="text" name="username" placeholder="Nom d'utilisateur" />
+                <input class="field" id="field-password" type="password" name="password" placeholder="Mot de passe" />
+                <input class="field" id="field-email"    type="text" name="email" placeholder="Adresse email" />
+                <button class="button" type="submit" value="Créer son propre compte">
+                    Terminer
+                </button>
+            </form>
+        </div>
+
+        <p class="center">
             Si vous avez déjà un compte cliquez
             <a title="Créer un compte" href="login.php">
                 ici
