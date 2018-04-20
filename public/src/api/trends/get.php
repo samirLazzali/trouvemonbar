daf9500c1c3f2d34466990d7968e6a8a6a51a5d3
T. Kowalski <thomaskowalski@outlook.com>
2018-04-20 10:44:48 +0200

    Ajout de la recherche de tendances sur une période donnée.

---
+++ public/src/api/trends/get.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Trouve les tendances du réseau.
+ * Méthode : GET
+ * Paramètres :
+ * - timespan : la période (en secondes) sur laquelle chercher les tendances
+ * - limit    : le nombre maximal de tendances à renvoyer
+ * Renvoie :
+ * - status = success, <Dictionnaire de tendances sous la forme { '#hashtag' : nb_de_tweets, ... }>
+ */
+
+
+require_once("../../config.php");
+require_once("Trend.php");
+
+if (isset($_GET['timespan']))
+    $timespan = $_GET['timespan'];
+else
+    $timespan = 3600;
+
+if (isset($_GET['limit']))
+    $limit = $_GET['limit'];
+else
+    $limit = 10;
+
+$trends = Trend::getTrends($limit, $timespan);
+success_die($trends);
---
+++ public/src/classes/Trend.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Class Trend
+ * Décrit une tendance.
+ */
+
+class Trend
+{
+    /** @var int le nombre de tweets avec ce hashtag */
+    private $postCount;
+    /** @var array les tweets avec ce hashtag */
+    private $posts = null;
+
+    /**
+     * Trouve les tendances du réseau.
+     * @param int $limit le nombre maximal de hashtags à renvoyer
+     * @param int $timelimit la fenetre de temps où rechercher les tendances (en secondes)
+     * @return array les tendances sous forme d'array de Trend
+     */
+    public static function getTrends($limit = 5, $timelimit = 3600)
+    {
+        $SQL = "SELECT * FROM " . TABLE_Posts . " WHERE Timestamp >= :timestamp";
+        $db = connect();
+        $statement = $db->prepare($SQL);
+        $statement->bindValue(":timestamp", $timelimit == 0 ? 0 : time() - $timelimit);
+        $statement->execute();
+
+        $rows = $statement->fetchAll();
+        $result = array();
+
+        if (!$rows)
+            return $result;
+
+        $hashtagCount = array();
+        foreach($rows as $row)
+        {
+            $post = Post::fromRow($row);
+            $hashtags = self::getHashtags($post);
+
+            foreach($hashtags as $hashtag)
+            {
+                if (isset($hashtagCount[$hashtag]))
+                    $hashtagCount[$hashtag]++;
+                else
+                    $hashtagCount[$hashtag] = 1;
+            }
+        }
+
+
+        asort($hashtagCount);
+
+        if ($limit == 0)
+            return $hashtagCount;
+
+        $hashtagCount = array_reverse($hashtagCount);
+        $hashtagCount = array_slice($hashtagCount, 0, $limit);
+        return $hashtagCount;
+    }
+
+    private static function getHashtags($post)
+    {
+        $words = explode(" ", $post->getContent());
+        $hashtags = array();
+        foreach($words as $word)
+        {
+            if (substr($word, 0, 1) == "#")
+                array_push($hashtags, $word);
+        }
+
+        return array_unique($hashtags);
+    }
+}
