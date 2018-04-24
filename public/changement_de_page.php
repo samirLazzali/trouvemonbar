768663d9d0ed5b57e4758adb5b351029b1c4672f
dodlaugier <dodlaugier@gmail.com>
2018-04-24 17:53:20 +0200

    oui

--- public/changement_de_page.php
+++ public/changement_de_page.php
@@ -1,6 +1,6 @@
 <?php
 if(isset($_POST['cac']) AND ($_POST['acc'] == 'Accueil')) { header("location:index.php"); }
-if(isset($_POST['ap']) AND ($_POST['ap'] == 'Apéral')) { header("location:Aperal.php"); }
+if(isset($_POST['ap']) AND ($_POST['ap'] == 'Apéral')) { header("location:a_propos_aperal.php"); }
 if(isset($_POST['oe']) AND ($_POST['oe'] == 'Oenologiie')) { header("location:a_propos_oeno.php"); }
 if(isset($_POST['reu']) AND ($_POST['reu'] == 'Réunion')) { header("location:oenologie.php"); }
 if(isset($_POST['clas']) AND ($_POST['clas'] == 'Classement')) { header("location:classement.php"); }
