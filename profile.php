e267ccb76716bdebeba3b2899675358eb7ae0299
LAFOSSE Matthieu <matthieu.lafosse@outlook.com>
2018-05-10 18:10:05 +0200

    Commit V0 menu

---
+++ profile.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <link rel="stylesheet" type="text/css" href="css/template.css">
+    <meta charset="UTF-8">
+    <title>Page d'acceuil</title>
+</head>
+<body>
+<a href="index.html"><img src="img/logo.png" alt="Logo" height ="88" width="94"></a>
+<ul>
+    <li><a class="active" href="index.html">Home</a></li>
+    <li><a href="posts.php">My posts</a></li>
+    <li><a class="active" href="profile.php">Profile</a></li>
+    <li class="dc"><a href="disconnect.php">Disconnect</a></li>
+</ul>
+</body>
+</html>
