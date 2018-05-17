4c3d88b29c0a0eabcaa4ad383640b1363edc048b
Ruitong XU <xuruitonglea@hotmail.com>
2018-05-17 16:12:25 +0000

    Update File.php

--- src/app/models/File.php
+++ src/app/models/File.php
@@ -12,4 +12,49 @@ namespace User;
 class File
 {
 
+
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * @param $filename
+     * @param $filehash
+     * @param bool $private false
+     * @param null $extension
+     * @return int db()->lastInsertId() id of the file which was just upload if successful
+     */
+    public static function insert_file($filename,$filehash,$extension=null,$private=false){
+
+        $query = db()->prepare("INSERT INTO file (filename, filehash, extension) VALUES  (?, ?, ?)");
+        $success = $query->execute([$filename,$filehash,$extension]);
+
+        if($success)
+            return db()->lastInsertId("file_fileid_seq");
+        else
+            return false;
+    }
+
+
+    /**
+     * @return array list of all files in the database
+     */
+    public static function filelist()
+    {
+
+        $query = db()->prepare("SELECT fileid, filename FROM file");
+        $query->execute();
+
+        return $query->fetchAll();
+    }
+
+    /**
+     * @param $filepath
+     * @return file hashcode
+     */
+    public static function filehash($filepath)
+    {
+        return hash_file("md5",$filepath);
+    }
 }
