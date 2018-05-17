3a41963439e8f16801e1baae672a70573c7a3d3e
Ruitong XU <xuruitonglea@hotmail.com>
2018-05-17 16:08:10 +0000

    Update upload_file.php

--- public/upload_file.php
+++ public/upload_file.php
@@ -6,6 +6,9 @@
  * Time: 10:01
  *
  */
+require "../src/app/helpers.php";
+include "../src/app/models/File.php";
+
 
 header("content-type:text/html;charset:utf-8");
 $fileInfo = $_FILES["myfile"];
@@ -17,7 +20,7 @@ $size = $fileInfo["size"];
 $tmp_name = $fileInfo["tmp_name"];
 $maxSize=2*1024*1024;// the maximum
 $allowExt=array("txt","pdf","png","jpg");
-//$flag = true;//if the
+
 //for Judging error number
 if($error == 0){
     //
@@ -31,28 +34,27 @@ if($error == 0){
     if(!in_array($ext,$allowExt)){
         exit("the file is not correct");
     }
-
-    /*
-    if($flag){
-        if(@!getimagesize($tmp_name)){
-            exit("");
-        }
-    }
-*/
+    
     //create the path
     $path = "../documents/";
     if(!file_exists($path)){
         mkdir($path,0777,true);
         chmod($path,0777);
     }
-
-    //ensure the name is the only
-    $uniName = md5(uniqid(microtime(true),true)).".".$ext;
+    
     $destination = $path.$filename;
 
     if(@move_uploaded_file($tmp_name,$destination)){
 
-        echo "upload successed";
+        $filehash=User\File::filehash($destination);
+
+        if($id=User\File::insert_file($filename,$filehash))
+        {
+            $newname=$path.$id.$filename;
+            rename($destination,$newname);
+            echo "upload successed";}
+        else
+            echo "upload failed";
     }else{
 
         echo "upload failed";
