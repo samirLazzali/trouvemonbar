b29d501e2d675f3759931434148b4622d26db271
simon_lauzeral <sim78100@hotmail.fr>
2018-05-18 17:22:38 +0200

    toute les pages sont faites

--- public/changement_de_page.php
+++ public/changement_de_page.php
@@ -5,7 +5,7 @@ if(isset($_POST['ap']) AND ($_POST['ap'] == 'Apéral')) { header("location:a_pro
 if(isset($_POST['oe']) AND ($_POST['oe'] == 'Oenologiie')) { header("location:a_propos_oeno.php"); }
 if(isset($_POST['reu']) AND ($_POST['reu'] == 'Réunion')) { header("location:reunion.php"); }
 if(isset($_POST['clas']) AND ($_POST['clas'] == 'Classement')) { header("location:classement.php"); }
-if(isset($_POST['adm']) AND ($_POST['adm'] == 'Admin')) { header("location:Admin.php"); }
+if(isset($_POST['adm']) AND ($_POST['adm'] == 'Admin')) { header("location:admin.php"); }
 
 //menu connection
 if(isset($_POST['co']) AND ($_POST['co'] == "Se connecter")) { header("location:connexion.php"); }
