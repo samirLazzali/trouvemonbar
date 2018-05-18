3e09c2f62b3c81dc3c82bffe6e411d5e288fd088
Julien CHAMAYOU <julien.chamayou@outlook.fr>
2018-05-18 19:36:47 +0000

    Replace profil.php

--- Julien/profil.php
+++ Julien/profil.php
@@ -1,15 +1,19 @@
 <?php
-require '../vendor/autoload.php';
+session_start();
+
+require_once '../vendor/autoload.php';
+require_once 'Vue.php';
+
 //postgres
 $dbName = getenv('DB_NAME');
 	$dbUser = getenv('DB_USER');
 	$dbPassword = getenv('DB_PASSWORD');
 	$connection = new PDO("pgsql:host=postgres user=$dbUser dbname=$dbName password=$dbPassword");
-    $userRepository = new User\UserRepository($connection);
-	$amisRepository = new Amis\AmisRepository($connection);
-	$tweetRepository = new Tweet\TweetRepository($connection);
-	$tweets=$tweetRepository->fetchAll();
-	$messageRepository = new Message\MessageRepository($connection);
+    //$userRepository = new User\UserRepository($connection);
+	//$amisRepository = new Amis\AmisRepository($connection);
+	//$tweetRepository = new Tweet\TweetRepository($connection);
+	//$tweets=$tweetRepository->fetchAll();
+	//$messageRepository = new Message\MessageRepository($connection);
 	$tweetManager = new Tweet\TweetManager($connection);
 
 if(isset($_POST['visite'])){ // si formulaire soumis
@@ -20,56 +24,78 @@ else{
 
 }
 
-$sth = $connection->prepare('SELECT id FROM "user" WHERE firstname=\''.$pseudo.'\'');
+/*$sth = $connection->prepare('SELECT id FROM "user" WHERE firstname=\''.$pseudo.'\'');
     $sth->execute();
-    $result = $sth->fetch(PDO::FETCH_OBJ);
+    $result = $sth->fetch(PDO::FETCH_OBJ);*/
 
-$id = $result->id;
+$id = idUser($pseudo);
 
-	
+/*
+if(!$result){
+    print "Profil Introuvable";
+}
+*/
+
+
+
+enTete("Profil de $pseudo", "CSS/style.css");
+afficheMenu();
+
+titreH1("Profil de $pseudo");
+
+
+
+
 ?>
+<script src="fonctionsJS.js"></script>
 
-<html>
-	<head>
-		
-	</head>
-	<body>
 
-<?php
 
-if(!empty($pseudo)){
-	$sth = $connection->prepare('SELECT firstname FROM "user"');
-    $sth->execute();
-    $result = $sth->fetch(PDO::FETCH_OBJ);
-    while($result){
-      	if($pseudo==$result->firstname){
-      		print "Profil de $pseudo : </br> Dernier Tweets </br>";
-			$tweets = $tweetManager->get($id);
-			$tweet = $tweets->fetch(PDO::FETCH_OBJ);
-			while($tweet){
 
-				$tweetManager->show_tweet($pseudo,$tweet);
-				$tweet = $tweets->fetch(PDO::FETCH_OBJ);
-			}
-			break;
-      	}
-        $result = $sth->fetch(PDO::FETCH_OBJ);
-    }
+<?php 
+print "<center>";
 
-    if(!$result){
-    		print "Profil Introuvable";
-    }
-	
+if(!is_ami($_SESSION['id'],$id) && $id!=$_SESSION['id'] ){
+	print "<div class=\"informations\">";
+	print "Nombre d'amis : ";
+	echo getNbAmis($id);
+	print " Nombre de tweets : ";
+	echo getNbTweet($id);
+	print "<br/>";
+	print "<form id='ajouteramis' method='post' action='ajouteramis.php'>";
+	print "<input type='hidden' name='personne' value=\"$id\"></input><br/>";
+    print "<input  type='submit' value='Ajouter' onclick='alert(\"Ami ajouté !\");'>";
+    print "</form>";
+    print "</div>";
 }
 
+else if($id!=$_SESSION['id']){
+	print "<div class=\"informations\">";
+	print "Nombre d'amis : ";
+	echo getNbAmis($id);
+	print " Nombre de tweets : ";
+	echo getNbTweet($id);
+	print "<br/>";
+	print "<form id='supprimer' method='post' action='supprimeramis.php'>";
+	print "<input type='hidden' name='personne' value=\"$id\"></input><br/>";
+    print "<input  type='submit' value='Supprimer' onclick='alert(\"Ami supprimé !\");'>";
+    print "</form>";
+    print "</div>";
+   
+	
+}
+ 
+if(!empty($pseudo)){
+	print "<br/>";
+    $tweets = getTweetId($id);
+    afficheListeTweets($tweets);
+     print "</center>";
+}
 else{
 		print "Champ Vide";
 }
 
-	
+
+
+pied();
 ?>
-	<br/>
-	
-	<a href="accueil.php">Retour </a>
-	</body>
-</html>
