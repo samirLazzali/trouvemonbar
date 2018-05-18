a18c347d5d39a54f6b3ce645e1aeceaee83dbae0
simon_lauzeral <sim78100@hotmail.fr>
2018-05-18 16:37:20 +0200

    edit menu

--- public/menu.php
+++ public/menu.php
@@ -13,17 +13,26 @@ function menu_navigation()
 
 function menu_connexion(){
     echo '<form method = "post" action = "changement_de_page.php" id="boutons_connexion">';
-    echo '<div classe="bouton"><input type="submit" class="button" name="co" value="Se connecter" ></div>';
-    echo '<div classe="bouton"><input type="submit" class="button" name="ins" value="inscription" ></div>';
+    echo '<div class="bouton"><input type="submit" class="button" name="co" value="Se connecter" ></div>';
+    echo '<div class="bouton"><input type="submit" class="button" name="ins" value="inscription" ></div>';
     echo '</form>';
 }
 
 function sous_menu_aperal(){
     echo '<form method = "post" class="sous_menu" action ="changement_de_page.php" id="boutons_aperal">';
-    echo '<div classe="bouton"><input type="submit" class="button" name="propA" value="A propos" ></div>';
-    echo '<div classe="bouton"><input type="submit" class="button" name="prepA" value="Préparatif" ></div>';
-    echo '<div classe="bouton"><input type="submit" class="button" name="rec" value="Recettes" ></div>';
-    echo '<div classe="bouton"><input type="submit" class="button" name="inta" value="Intendance" ></div>';
-    echo '<div classe="bouton"><input type="submit" class="button" name="ava" value="Avis" ></div>';
+    echo '<div class="bouton"><input type="submit" class="button" name="propA" value="A propos" ></div>';
+    echo '<div class="bouton"><input type="submit" class="button" name="prepA" value="Préparatif" ></div>';
+    echo '<div class="bouton"><input type="submit" class="button" name="rec" value="Recettes" ></div>';
+    echo '<div class="bouton"><input type="submit" class="button" name="inta" value="Intendance" ></div>';
+    echo '<div class="bouton"><input type="submit" class="button" name="ava" value="Avis" ></div>';
+    echo '</form>';
+}
+
+function sous_menu_oenologie(){
+    echo '<form method = "post" class="sous_menu" action ="changement_de_page.php" id="boutons_aperal">';
+    echo '<div class="bouton"><input type="submit" class="button" name="propO" value="A propos" ></div>';
+    echo '<div class="bouton"><input type="submit" class="button" name="prepO" value="Préparatif" ></div>';
+    echo '<div class="bouton"><input type="submit" class="button" name="avO" value="Avis" ></div>';
+    echo '<div class="bouton"><input type="submit" class="button" name="vin" value="Liste des vins" ></div>';
     echo '</form>';
 }
