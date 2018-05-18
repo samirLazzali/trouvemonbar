28fd464055531b045095934f785dde38f0406f41
simon_lauzeral <sim78100@hotmail.fr>
2018-05-18 16:18:16 +0200

    reunion

---
+++ public/reunion.php
@@ -0,0 +1,27 @@
+<?php
+require '../vendor/autoload.php';
+include('menu.php');
+//postgres
+$dbName = getenv('DB_NAME');
+$dbUser = getenv('DB_USER');
+$dbPassword = getenv('DB_PASSWORD');
+$connection = new PDO("pgsql:host=postgres user=$dbUser dbname=$dbName password=$dbPassword");
+
+$userRepository = new \User\UserRepository($connection);
+$users = $userRepository->fetchAll();
+?>
+
+<html>
+<head>
+    <title> réunion </title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css"  href="style_index.css">
+</head>
+<div class="banniere">
+    <?php
+    menu_connexion();
+    menu_navigation()
+    ?>
+</div>
+
