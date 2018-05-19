96d662f41dd204d55fa1db3d776ad7fa63faf553
Martin Dufour <martindufour@free.fr>
2018-05-19 09:47:33 +0000

    Update db.sql

--- data/db.sql
+++ data/db.sql
@@ -1,4 +1,44 @@
-CREATE TABLE evenements(
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:8889
+-- Généré le :  sam. 19 mai 2018 à 09:33
+-- Version du serveur :  5.6.38
+-- Version de PHP :  7.2.1
+
+/* SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO"; */
+time_zone := "+00:00";
+
+--
+-- Base de données :  `projetWeb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `categories`
+--
+
+CREATE TABLE categories (
+  id int NOT NULL,
+  categorie varchar(100) NOT NULL
+) ;
+
+--
+-- Déchargement des données de la table `categories`
+--
+
+INSERT INTO categories (id, categorie) VALUES
+(1, 'club Brn');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `evenements`
+--
+
+CREATE TABLE evenements (
   id int NOT NULL,
   organisateur int NOT NULL,
   nom varchar(100) NOT NULL,
@@ -12,32 +52,59 @@ CREATE TABLE evenements(
   musique int DEFAULT NULL,
   categorie int DEFAULT NULL,
   table_participants varchar(100) NOT NULL
-);
+) ;
+
+--
+-- Déchargement des données de la table `evenements`
+--
 
 INSERT INTO evenements (id, organisateur, nom, description, lieu, date, date_creation, date_modif, before, prix, musique, categorie, table_participants) VALUES
 (4, 1, 'First Event', NULL, 'Chez moi', '2018-05-08', '2018-05-08', NULL, NULL, 0, NULL, 1, 'participants3897910314'),
 (5, 1, 'First Event', NULL, 'Chez moi', '2018-05-09', '2018-05-09', NULL, NULL, 0, NULL, 1, 'p2411932048');
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `musiques`
+--
 
 CREATE TABLE musiques (
   id int NOT NULL,
   musique varchar(100) NOT NULL
 ) ;
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `p2411932048`
+--
 
 CREATE TABLE p2411932048 (
   id int NOT NULL
 ) ;
 
+--
+-- Déchargement des données de la table `p2411932048`
+--
 
 INSERT INTO p2411932048 (id) VALUES
 (1);
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `participants3897910314`
+--
 
 CREATE TABLE participants3897910314 (
   id int NOT NULL
 ) ;
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
 
 CREATE TABLE users (
   id int NOT NULL,
@@ -50,43 +117,53 @@ CREATE TABLE users (
   id_groupe int NOT NULL
 ) ;
 
+--
+-- Déchargement des données de la table `users`
+--
+
 INSERT INTO users (id, firstname, lastname, birthday, nickname, domicile, mdp, id_groupe) VALUES
 (1, 'Martin', 'Dufour', '1997-08-27', 'Feuj', 'maison', 'martin', 2);
 
+--
+-- Index pour les tables déchargées
+--
 
+--
+-- Index pour la table `categories`
+--
 ALTER TABLE categories
   ADD PRIMARY KEY (id);
 
 --
--- Index pour la table evenements
+-- Index pour la table `evenements`
 --
 ALTER TABLE evenements
   ADD PRIMARY KEY (id),
-  ADD KEY organisateur_id (organisateur),
-  ADD KEY musique_id (musique),
-  ADD KEY categorie_id (categorie),
-  ADD KEY participants_table_nom (table_participants);
+  ADD KEY `organisateur_id` (organisateur),
+  ADD KEY `musique_id` (musique),
+  ADD KEY `categorie_id` (categorie),
+  ADD KEY `participants_table_nom` (table_participants);
 
 --
--- Index pour la table musiques
+-- Index pour la table `musiques`
 --
 ALTER TABLE musiques
   ADD PRIMARY KEY (id);
 
 --
--- Index pour la table p2411932048
+-- Index pour la table `p2411932048`
 --
 ALTER TABLE p2411932048
   ADD PRIMARY KEY (id);
 
 --
--- Index pour la table participants3897910314
+-- Index pour la table `participants3897910314`
 --
 ALTER TABLE participants3897910314
   ADD PRIMARY KEY (id);
 
 --
--- Index pour la table users
+-- Index pour la table `users`
 --
 ALTER TABLE users
   ADD PRIMARY KEY (id);
@@ -96,25 +173,25 @@ ALTER TABLE users
 --
 
 --
--- AUTO_INCREMENT pour la table categories
+-- AUTO_INCREMENT pour la table `categories`
 --
 ALTER TABLE categories
   MODIFY id cast(11 as int) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT pour la table evenements
+-- AUTO_INCREMENT pour la table `evenements`
 --
 ALTER TABLE evenements
   MODIFY id cast(11 as int) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT pour la table musiques
+-- AUTO_INCREMENT pour la table `musiques`
 --
 ALTER TABLE musiques
   MODIFY id cast(11 as int) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT pour la table users
+-- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE users
   MODIFY id cast(11 as int) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
