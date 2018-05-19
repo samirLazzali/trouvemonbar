0b35f47b0ed34eed3d8543422c2954430339359f
Joséphine Barthel <josephine.barthel11@gmail.com>
2018-05-19 16:33:57 +0200

    added info page, removed useless file

--- public/view_game.php
+++ public/view_game.php
@@ -12,31 +12,61 @@
 
 require "../src/app/helpers.php";
 
-$gameid =$_GET['id'];
 
-// Si le paramètre id est manquant ou invalide
-if(empty($gameid) or !is_numeric( $gameid) or !isset($gameid))
-    error("404");
+if(Auth::logged()) {
 
+    $gameid = $_GET['id'];
+
+    // Si le paramètre id est manquant ou invalide
+    if (empty($gameid) or !is_numeric($gameid) or !isset($gameid))
+        error("404");
+
+    else {
+
+        try {
+            $game = new Game($gameid);
+            $creator = new User($game->getCreator());
+        } catch (Exception $e) {
+            error("500", $e->getMessage());
+        }
+
+        $oneshots = $game->one_shot_schedules();
+        $reccurrents = $game->reccurrent_schedules();
+        $comments = $game->comments();
+        $players = $game->players();
+        $isOwner = $game->getCreator() == $_SESSION['user'];
+
+        $layout = new Layout("users");
+        include view("game_view.php");
+        $layout->show($game->getName());
+
+    }
+}
 else {
+    $gameid = $_GET['id'];
 
-    try {
-        $game = new Game($gameid);
-        $creator = new User($game->getCreator());
-    }catch(Exception $e)
-    {
-        error("500", $e->getMessage());
+    // Si le paramètre id est manquant ou invalide
+    if (empty($gameid) or !is_numeric($gameid) or !isset($gameid))
+        error("404");
+
+    else {
+
+        try {
+            $game = new Game($gameid);
+            $creator = new User($game->getCreator());
+        } catch (Exception $e) {
+            error("500", $e->getMessage());
+        }
+
+        $oneshots = $game->one_shot_schedules();
+        $reccurrents = $game->reccurrent_schedules();
+        $comments = $game->comments();
+        $players = $game->players();
+        $isOwner = false;
+
+        $layout = new Layout("visitors");
+        include view("game_view.php");
+        $layout->show($game->getName());
     }
-
-    $oneshots = $game->one_shot_schedules();
-    $reccurrents = $game->reccurrent_schedules();
-    $comments = $game->comments();
-    $players = $game->players();
-    $isOwner = $game->getCreator() == $_SESSION['user'];
-
-    $layout = new Layout("users");
-    include view("game_view.php");
-    $layout->show($game->getName());
-
-    }
+}
 
