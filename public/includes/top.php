e76f3bd9c4b829c840e531989cfc702bf6d43ca4
Fontaine <adrien.vizier@gmail.com>
2018-05-20 18:07:57 +0200

    Début CSS

--- public/includes/top.php
+++ public/includes/top.php
@@ -1,36 +1,40 @@
 <!DOCTYPE html>
 <html>
-	<head>
-	<?php
-	
-	if(isset($titre) && trim($titre) != '')
-	$titre = $titre.' : '.TITRESITE;
-	
-	else
-	$titre = TITRESITE;
-	?>
-		<title><?php echo $titre; ?></title>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-		<meta name="language" content="fr" />
-	</head>
-
-
-	<body>
-		// Mettre la bannière ici 
-		<div id="menu">
-			<?php
-			if(isset($_SESSION['id_user']))
-			{
-			?>
-				<a href="<?php echo ROOTPATH; ?>/index.php">Accueil</a>   &nbsp;<a href="<?php echo ROOTPATH; ?>/membres/moncompte.php">Gérer mon compte</a>   &nbsp;<a href="<?php echo ROOTPATH; ?>/membres/deconnexion.php">Se déconnecter</a>
-			<?php
-			}
-			
-			else
-			{
-			?>
-				<a href="<?php echo ROOTPATH; ?>/index.php">Accueil</a>   &nbsp;<a href="<?php echo ROOTPATH; ?>/membres/inscription.php">Inscription</a>   &nbsp;<a href="<?php echo ROOTPATH; ?>/membres/connexion.php">Connexion</a>
-			<?php
-			}
-			?>
-		</div>
+  <head>
+    <?php
+       
+       if(isset($titre) && trim($titre) != '')
+       $titre = $titre.' : '.TITRESITE;
+       
+       else
+       $titre = TITRESITE;
+       ?>
+    <title><?php echo $titre; ?></title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="language" content="fr" />
+    <link rel="stylesheet" type="text/css" href="../style.css"/>
+  </head>
+  
+  
+  <body>
+    <div id="bandeau">
+      CATISFACTION
+    </div>
+    <div id="menu">
+      <?php
+	 if(isset($_SESSION['id_user']))
+	 {
+	 ?>
+      <a href="<?php echo ROOTPATH; ?>/index.php">Accueil</a>   &nbsp;<a href="<?php echo ROOTPATH; ?>/membres/moncompte.php">Gérer mon compte</a>   &nbsp;<a href="<?php echo ROOTPATH; ?>/membres/deconnexion.php">Se déconnecter</a>
+      <?php
+	 }
+	 
+	 else
+	 {
+	 ?>
+      <a href="<?php echo ROOTPATH; ?>/index.php">Accueil</a>   &nbsp;<a href="<?php echo ROOTPATH; ?>/membres/inscription.php">Inscription</a>   &nbsp;<a href="<?php echo ROOTPATH; ?>/membres/connexion.php">Connexion</a>
+      <?php
+	 }
+	 ?>
+      
+    </div>
--- public/membres/inscription.php
+++ public/membres/inscription.php
@@ -24,14 +24,43 @@ include('../includes/top.php');
 			Merci de remplir ces champs pour continuer.</p>
 			<form action="champs_inscription.php" method="post" name="Inscription">
 				<fieldset><legend>Renseignements</legend>
-					<label for="pseudo" class="float">Nom d'utilisateur :&nbsp;        &nbsp;          &nbsp;      &nbsp;     &nbsp;     &nbsp;      &nbsp;     &nbsp;</label> <input type="text" name="login" id="login" size="30" /><em>&nbsp; (compris entre 3 et 32 caractères)</em><br/>
-					<label for="mdp" class="float">Mot de passe :     &nbsp;     &nbsp;      &nbsp;       &nbsp;         &nbsp;          &nbsp;      &nbsp;     &nbsp;     &nbsp;      &nbsp;     &nbsp;</label> <input type="password" name="password" id="password" size="30" /><em>&nbsp; (compris entre 4 et 50 caractères, avec au moins un chiffre et une majuscule)</em><br/>
-					<label for="mdp_verif" class="float">Mot de passe (vérification) :</label> <input type="password" name="password_verif" id="password_verif" size="30" /><br/>
-					<label for="mail" class="float">Mail :&nbsp;          &nbsp;      &nbsp;    &nbsp;        &nbsp;          &nbsp;      &nbsp;     &nbsp;     &nbsp;      &nbsp;     &nbsp;&nbsp;        &nbsp;          &nbsp;      &nbsp;     &nbsp;     &nbsp;      &nbsp;     &nbsp;</label> <input type="text" name="mail" id="mail" size="30" /> <br/>
-					<label for="mail_verif" class="float">Mail (vérification) :         &nbsp;      &nbsp;    &nbsp;        &nbsp;          &nbsp;      &nbsp;     &nbsp;</label> <input type="text" name="mail_verif" id="mail_verif" size="30" /><br/>
-					<label for="phone_number" class="float">Numéro de téléphone :&nbsp;    &nbsp;     &nbsp;      &nbsp;     &nbsp;</label> <input type="integer" name="phone_number" id="phone_number" size="30" /><br/>
-					<br/>
-					<div class="center"><input type="submit" value="Valider" /></div>
+				<table>
+					<tr>
+					<td><label for="pseudo" class="float">Nom d'utilisateur :</label></td>
+					<td><input type="text" name="login" id="login" size="30" /></td>
+					<td><em> (compris entre 3 et 32 caractères)</em></td>
+					</tr>
+					<tr>
+					<td><label for="mdp" class="float">Mot de passe :</label></td>
+					<td><input type="password" name="password" id="password" size="30" /></td>
+					<td><em>(compris entre 4 et 50 caractères, avec au moins un chiffre et une majuscule)</em></td>
+					</tr>
+					<tr>
+					<td><label for="mdp_verif" class="float">Mot de passe (vérification) :</label></td>
+					<td><input type="password" name="password_verif" id="password_verif" size="30" /></td>
+					<td></td>
+					</tr>
+					<tr>
+					<td><label for="mail" class="float">Mail :</label></td>
+					<td><input type="text" name="mail" id="mail" size="30" /></td>
+					<td></td>
+					</tr>
+					<tr>
+					<td><label for="mail_verif" class="float">Mail (vérification) :</label></td>
+					<td><input type="text" name="mail_verif" id="mail_verif" size="30" /></td>
+					<td></td>
+					</tr>
+					<tr>
+					<td><label for="phone_number" class="float">Numéro de téléphone :</label></td>
+					<td><input type="integer" name="phone_number" id="phone_number" size="30" /></td>
+					<td></td>
+					</tr>
+					<tr>
+					<td><input type="submit" value="Valider" /></td>
+					<td></td>
+					<td></td>
+					</tr>
+				</table>
 				</fieldset>
 			</form>
 		</div>
