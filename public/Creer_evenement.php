4e837b4e9e5902d8f0852b20a45e143c138d6c78
Christian Morello <christian.morello@ensiie.fr>
2018-05-20 23:17:31 +0200

    un début de css

--- public/Creer_evenement.php
+++ public/Creer_evenement.php
@@ -6,6 +6,7 @@
 <head>
 	<title> Créez votre évènement !  </title>
 	<metac charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="Creer_evenement.css">
 	<link rel="stylesheet" type="text/css" href="main.css">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 	
@@ -14,27 +15,27 @@
 <?php include "upperBar.php" ?>
 <center><h2> Faites profiter les autres en créant votre évènement !</h2>
      	    <br/><br/>
-	     <form action="Creer_evenement.php" method="post">
+	     <form action="Creer_evenement.php" method="post" id="event">
 	     	   <label for="nom_event"><b>Nom de l'évènement</b></label>
 		   <br/>
-	     	   <input type="text" name="nom_event"/>
+	     	   <input type="text" name="nom_event" required/>
 		   <br/>
 		   <br/>
 		   <label for="date_event"><b>Quand?</b></label>
 		   <br/>
-		   <input type="date" name="date_event"></form>
+		   <input type="date" name="date_event" required>
 		   <br/>
 		   <label for="lieu_event"><b>Où?</b></label>
 		   <br/>
-		   <input type="text" name="lieu_event">
+		   <input type="text" name="lieu_event" required>
 		   <br/>
 		   <br/>
 		   <label for="before"><b>Quel before chacal?<br/></b><label/>
-		   <input type="text" name="before">
+		   <input type="text" name="before" required>
 		   <br></br>
 		   <label for="prix"><b>Et ça coûte combien?<br/></b><label/>
 		   <br/>
-		   <input type="text" name="prix">
+		   <input type="text" name="prix" required>
 		   <br></br><br>
 		   <label for="image"><b> Une petite photo de l'endroit?<br/></b><label/>
 		   <br/>
@@ -42,6 +43,7 @@
 		   <br/>
 		   <br/>
 		   <input type="submit" value="Validez vos informations">
+		   </form>
 		   
 		   
 		   
