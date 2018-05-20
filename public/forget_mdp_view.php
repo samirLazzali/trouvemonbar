dae08adbb8c65fb24e67c74ca488717ae07e474e
Ibtissam Liedri <ibtissamliedri@outlook.fr>
2018-05-20 14:31:51 +0000

    Add new file: forget_mdp_view

---
+++ public/forget_mdp_view.php
@@ -0,0 +1,5 @@
+<h1> Réinitialiser votre mot de passe</h1>
+<form action ="actions/new_mdp_action.php" method="post">
+    Email:<input type="text" name = "email" /></br>
+    <input type ="submit" name ="submit" />
+</form>
