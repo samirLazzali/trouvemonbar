1af5eb0ffc531e04e181306e96153cfa16581e8f
yassirchekour <yassir-chekour@hotmail.fr>
2018-05-20 19:50:30 +0200

    SideBar Modification

--- src/sidebar.php
+++ src/sidebar.php
@@ -6,8 +6,6 @@ function dispSidebar() {
     print "<div class=sidenav>";
     print "<a class=m-link href=\"perso.php\">Mon Profil</a></br>";
     print "<a class=m-link href=\"createForm.php\">Nouvelle annonce</a></br>";
-    print "<a class=m-link>Reporter</a></br>";
-    print "<a class=m-link>Rechercher</a></br>";
     print "</div>";
     print "</div>";
 }
