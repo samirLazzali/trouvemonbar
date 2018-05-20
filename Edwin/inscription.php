b5b0f9f010995b92e92fe43c2cdd03363e3213af
Mauillon <edwin.mauillon@wanadoo.fr>
2018-05-20 07:41:05 +0000

    Update inscription.php

--- Edwin/inscription.php
+++ Edwin/inscription.php
@@ -2,27 +2,38 @@
 // on teste si le visiteur a soumis le formulaire
 if (isset($_POST['inscription']) && $_POST['inscription'] == 'Inscription') {
 	// on teste l'existence de nos variables. On teste également si elles ne sont pas vides
-	if ((isset($_POST['login']) && !empty($_POST['login'])) && (isset($_POST['pass']) && !empty($_POST['pass'])) && (isset($_POST['pass_confirm']) && !empty($_POST['pass_confirm']))) {
+	if ((isset($_POST['login']) && !empty($_POST['login'])) && (isset($_POST['password']) && !empty($_POST['password'])) && (isset($_POST['pass_confirm']) && !empty($_POST['pass_confirm']))) {
 	// on teste les deux mots de passe
-	if ($_POST['pass'] != $_POST['pass_confirm']) {
+	if ($_POST['password'] != $_POST['pass_confirm']) {
 		$erreur = 'Les 2 mots de passe sont différents.';
 	}
 	else {
-		$base = mysql_connect ('serveur', 'login', 'password');
-		mysql_select_db ('nom_base', $base);
+		//postgres
+	$dbName = getenv('DB_NAME');
+	$dbUser = getenv('DB_USER');
+	$dbPassword = getenv('DB_PASSWORD');
+	try {
+		$connection = new PDO("pgsql:host=postgres user=$dbUser dbname=$dbName password=$dbPassword");
+	}
+	catch(Execption $e){
+		die('Erreur : '.$e->getMessage());
+	}
 
 		// on recherche si ce login est déjà utilisé par un autre membre
-		$sql = 'SELECT count(*) FROM user WHERE login="'.mysql_escape_string($_POST['login']).'"';
-		$req = mysql_query($sql) or die('Erreur SQL !<br />'.$sql.'<br />'.mysql_error());
-		$data = mysql_fetch_array($req);
+		$sql = $connection->prepare('SELECT count(*) FROM user WHERE login=?');
+		$sql->execute(array($_POST['login']));
+    	$result = $sql->fetch(PDO::FETCH_OBJ);
+		
 
-		if ($data[0] == 0) {
-		$sql = 'INSERT INTO membre VALUES("", "'.mysql_escape_string($_POST['login']).'", "'.mysql_escape_string(md5($_POST['pass'])).'")';
-		mysql_query($sql) or die('Erreur SQL !'.$sql.'<br />'.mysql_error());
+		if ($result[0] == 0) {
+		$sql =  $connection->prepare('INSERT INTO user(login,firstname,lastname,password) VALUES(?,?,?,?)');
+		$sql->execute(array($_POST['login'],$_POST['firstname'],$_POST['lastname'],$_POST['password']));
+    	$result = $sql->fetch(PDO::FETCH_OBJ);
+		//mysql_query($sql) or die('Erreur SQL !'.$sql.'<br />'.mysql_error());
 
 		session_start();
 		$_SESSION['login'] = $_POST['login'];
-		header('Location: acceuil.php');
+		header('Location: succes.php');\
 		exit();
 		}
 		else {
@@ -40,13 +51,16 @@ if (isset($_POST['inscription']) && $_POST['inscription'] == 'Inscription') {
 <head>
 <title>Inscription</title>
 </head>
-
+<h1>BIENVENU SUR TWITIIE </h1>
 <body>
-Inscription à l'espace membre :<br />
+Inscription à l'espace membre :<br/>
 <form action="inscription.php" method="post">
-Login : <input type="text" name="login"/><br />
-Mot de passe : <input type="password" name="pass"/><br />
-Confirmation du mot de passe : <input type="password" name="pass_confirm"/><br />
+<span class="formulaire"> Login : <input type="text" name="login"/><br/> </span>
+<span class="formulaire">Mot de passe : <input type="password" name="password"/><br/></span>
+<span class="formulaire">Confirmation du mot de passe : <input type="password" name="pass_confirm"/><br/> </span>
+<span class="formulaire">Nom : <input type="text" name="lastname"/><br/> </span>
+<span class="formulaire">Prenom : <input type="text" name="firstname"/><br/> </span>
+<span class="formulaire">Date de naissance : <input type="date(Y-m-d)" name="bday"/> <br/> </span>
 <input type="submit" name="inscription" value="Inscription">
 </form>
 <?php
