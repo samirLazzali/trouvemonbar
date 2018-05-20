4a99d78365b19e890941e07e5516b7f76dfccbb4
yassirchekour <yassir-chekour@hotmail.fr>
2018-05-20 14:56:56 +0200

    pull

---
+++ public/perso.php
@@ -0,0 +1,43 @@
+<?php
+//include("authentication.php");
+include("viewfunctions.php");
+include("annonce.php");
+include("sidebar.php");
+login();
+
+header_t("Les Bons Bails");
+if(!verif_authent()) { // si le gars est authentified ==>  acces aux offres
+    displayLogin();
+}
+if(verif_authent())
+{
+	dispSidebar();
+
+}
+
+//dispSidebar();
+?>
+
+<div class="main">
+
+<?php
+
+if(verif_authent()) { // si le gars est authentified ==>  acces aux offres
+    //indexco();
+
+    $annonces = Annonce::getAnnonces();
+    echo "<h2>Dernières Annonces</h2>";
+    foreach ($annonces as $an) {
+	$an->display();
+
+    }
+} else {	// sinon pas acces aux offres
+    indexnotco();
+}
+?>
+
+</div>
+
+<?php
+footer();
+?>
