52fd7108d3ac52457723b88c156c11cc641a74b4
Victor <victor.meas@gmail.com>
2018-05-20 18:40:02 +0000

    Update nondroit.php

--- public/nondroit.php
+++ public/nondroit.php
@@ -21,17 +21,16 @@ if(verif_authent())
 <div class="main">
 
 <div class=annonce>
-    <div class=errorimg> <img src="https://s2.qwant.com/thumbr/0x0/d/3/476c64f30da915f49e82e6cc5c90a4fb7dffb9a2951106386de84ee281a3ac/erroricon1.png?u=https%3A%2F%2Fblog.sqlauthority.com%2Fwp-content%2Fuploads%2F2016%2F01%2Ferroricon1.png&q=0&b=1&p=0&a=1" alt="error could not show img"> </div>
-	<div class=title style="text-align:center;"> <strong> UNABLE TO ACCESS QUERY </strong>
+	<div class=title style="text-align:center;"> <strong> Impossible d'acceder à votre demande </strong>
 	</div>
 	<div> 
 		<br>
-		Sorry, the content you're trying to access is unavailable.
+		Le contenu que vous essayez de visualiser n'est pas disponible.
 		<br>
 		<br>
-		You probably don't have the required rights to access them.
+		Vous n'y avez pas droit d'accès.
 		<br>
-		<a href="main.php"><strong> Go Back </strong></a>
+		<a href="main.php"><strong> Retour </strong></a>
 		<br>
 	</div>
 </div>
