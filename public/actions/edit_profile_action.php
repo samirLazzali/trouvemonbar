299b5b47348221b046ae7e407c4616329cddf78a
Tannk <xuruitognlea@hotmail.com>
2018-05-20 12:44:50 +0200

    update search_user.php

--- public/actions/edit_profile_action.php
+++ public/actions/edit_profile_action.php
@@ -7,8 +7,23 @@
  */
 require "../../src/app/helpers.php";
 
-$mail= htmlspecialchars($_POST['mail']);
-$password= htmlspecialchars($_POST['password']);
+if(isset($_POST["password"]))
+    $password = htmlspecialchars($_POST["password"]);
+else
+    $password = false;
+
+if(isset($_POST["gamesystem"]))
+    $gamesystemid = $_POST["gamesystem"];
+else
+    $gamesystemid = false;
+
+$mail= htmlspecialchars($_POST["mail"]);
+$nick= htmlspecialchars($_POST["nick"]);
+$lastname= htmlspecialchars($_POST["lastname"]);
+$firstname= htmlspecialchars($_POST["firstname"]);
+
+
+
 
 try {
     $user = new User($_GET['userid']);
@@ -17,15 +32,45 @@ catch(Exception $e)
 {
 
 }
-if($user->updateUser($password,$mail))
+
+if($nick == null)
 {
-    flash(" réussir à changer mot de passe et email ");
-    Auth::logout();
-    redirect("../authentication.php");
+    flash(" Pseudo ne peut pas être vide!!!");
+    redirect("../edit_profile.php");
+}
+if($mail == null)
+{
+    flash(" E-mail ne peut pas être vide!!!");
+    redirect("../edit_profile.php");
+}
+
+
+$masterylist=User::masterylist($user->getId());
+
+if($gamesystemid == false)
+    User::deleteMastery($user->getId());
+else
+{
+    if(count($masterylist) !== count($gamesystemid))
+        User::deleteMastery($user->getId());
+    foreach ($gamesystemid as $gamesystem)
+    {
+        $user::insertMastery($gamesystem,$user->getId());
+    }
+}
+
+if($user->updateUser_profile($nick,$firstname,$lastname,$mail,$password))
+{
+        flash(" réussir à changer votre profile ");
+        Auth::logout();
+        redirect("../authentication.php");
+}
+else {
+    flash("Erreur : le profile n'a pas pu être changé");
+    redirect("../edit_profile.php");
 
 
 }
-flash("Erreur : le profile n'a pas pu être changé");
-redirect("../edit_profile.php");
+
 
 
