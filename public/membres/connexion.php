216d274b5a523ffd9ee1188352cc6de8b810b699
Psiich0 <thomas.beratto@ensiie.fr>
2018-05-21 19:44:45 +0200

    Cette fois ci

--- public/membres/connexion.php
+++ public/membres/connexion.php
@@ -33,7 +33,7 @@ include('../includes/top.php');
 			prochain passage." pour laisser une trace sur votre ordinateur pour être
 			connecté automatiquement.<br/></p>
 			
-			<form name="connexion" id="connexion" method="post" action="connexion.php">
+			<form name="connexion" id="connexion" method="post" action="champs_connexion.php">
 				<fieldset><legend>Connexion</legend>
 				<table>
 					<tr>
@@ -56,90 +56,7 @@ include('../includes/top.php');
 			
 			<h2>Options</h2>
 			<p><a href="inscription.php">Je ne suis pas inscrit.</a><br/>
-			</p>
-			<?php
-
-else	{
-				$dbName = getenv('DB_NAME');
-				$dbUser = getenv('DB_USER');
-				$dbPassword = getenv('DB_PASSWORD');
-				$connexion = new PDO("pgsql:host=postgres user=$dbUser dbname=$dbName password=$dbPassword");
-				$result = $connexion->query("SELECT COUNT(id_user) AS nbr, id_user, login, password FROM Utilisateur WHERE
-											login = '".$_POST['login']."' GROUP BY id_user");
-				global $queries;
-				$queries++;
-				if($result->nbr == 1)
-				{
-					if(md5($_POST['password']) == $result->password)
-					{
-						$_SESSION['id_user'] = $result->id_user;
-						$_SESSION['login'] = $result->login;
-						$_SESSION['password'] = $result->password;
-						unset($_SESSION['connexion_login']);
-						
-						if(isset($_POST['cookie']) && $_POST['cookie'] == 'on')
-						{
-							setcookie('id_user', $result->id_user, time()+365*24*3600);
-							setcookie('password', $result->password, time()+365*24*3600);
-						}
-						
-						$informations = Array(
-										false,
-										'Connexion réussie',
-										'Vous êtes désormais connecté avec le nom d\'utilisateur <span class="login">'.htmlspecialchars($_SESSION['login'], ENT_QUOTES).'</span>.',
-										'',
-										ROOTPATH.'/index.php',
-										3
-										);
-						require_once('../information.php');
-						exit();
-					}
-					
-					else
-					{
-						$_SESSION['connexion_login'] = $_POST['login'];
-						$informations = Array(
-										true,
-										'Mauvais mot de passe',
-										'Vous avez fourni un mot de passe incorrect.',
-										' - <a href="'.ROOTPATH.'/index.php">Index</a>',
-										ROOTPATH.'/membres/connexion.php',
-										3
-										);
-						require_once('../information.php');
-						exit();
-					}
-				}
-				
-				else if($result->nbr > 1)
-				{
-					$informations = Array(
-									true,
-									'Doublon',
-									'Deux membres ou plus ont le même nom d\'utilisateur, contactez un administrateur pour régler le problème.',
-									' - <a href="'.ROOTPATH.'/index.php">Index</a>',
-									ROOTPATH.'/contact.php',
-									3
-									);
-					require_once('../information.php');
-					exit();
-				}
-				
-				else
-				{
-					$informations = Array(
-									true,
-									'Nom d\'utilisateur inconnu',
-									'Le nom d\'utilisateur <span class="login">'.htmlspecialchars($_POST['login'], ENT_QUOTES).'</span> n\'existe pas dans notre base de données. Vous avez probablement fait une erreur.',
-									' - <a href="'.ROOTPATH.'/index.php">Index</a>',
-									ROOTPATH.'/membres/connexion.php',
-									5
-									);
-					require_once('../information.php');
-					exit();
-				}
-			}
-			?>			
+			</p>		
 		</div>
 
 		<?php
