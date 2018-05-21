cb23ef7d9f5bd07b6e7410285e34db58f17c43e0
Paul <canat.paul@gmail.com>
2018-05-21 16:48:19 +0000

    Replace header.php

--- includes/header.php
+++ includes/header.php
@@ -3,4 +3,16 @@
     <link rel="stylesheet" type="text/css" href="css/relo.css">
     <meta charset="UTF-8">
     <title>Page d'acceuil</title>
+    <script>
+        function affiche(x) {
+            var elt = document.getElementById(x);
+            if(elt.style.display=="none") {
+                elt.style.display = "block";
+            }
+            else {
+                elt.style.display="none";
+            }
+        }
+    </script>
+    <?php include("fonctions.php")?>
 </head>
