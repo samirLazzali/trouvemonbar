6f80e90e067e7b1d98337ea6c6e283adc79d9632
Paul <canat.paul@gmail.com>
2018-05-21 17:26:19 +0000

    Replace debut.php

--- includes/debut.php
+++ includes/debut.php
@@ -6,6 +6,7 @@
     ?>
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="../css/template.css">
+    <link rel="stylesheet" type="text/css" href="../css/relo.css">
 </head>
 <?php
 
