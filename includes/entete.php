4284cf3ffa919e23309c6327a5e65445a3a8452b
Paul <canat.paul@gmail.com>
2018-05-21 16:38:01 +0000

    Add new file

---
+++ includes/entete.php
@@ -0,0 +1,6 @@
+<div id="cadre">
+    <h1 id="title">
+        <a href="index.php"><img class="home" src="img/logo.png" alt="Logo" height ="88" width="94"></a>
+        <span id="title1">GolrIIE</span>
+    </h1>
+</div>
