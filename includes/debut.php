62510ed19084a58b78df89a242c964eca3a788f8
LAFOSSE Matthieu <matthieu.lafosse@outlook.com>
2018-05-21 19:29:52 +0200

    bla bla

--- includes/debut.php
+++ includes/debut.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="../css/template.css">
+    <link rel="stylesheet" type="text/css" href="./css/template.css">
 </head>
 <?php
 
--- includes/mel.php
+++ includes/mel.php
@@ -1,7 +1,19 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Mattsune
- * Date: 21/05/2018
- * Time: 17:27
- */
+if ($id==0) erreur(ERR_IS_NOT_CO);
+?>
+
+<?php
+if (empty($_POST['Titre'])) // Si on la variable est vide, on peut considérer qu'on est sur la page de formulaire
+{
+    echo '<h1>Upload</h1>';
+    echo '<form method="post" action="mel.php" enctype="multipart/form-data">
+	<fieldset><legend>Infos</legend>
+	<label for="titre">* Titre :</label>  <input name="titre" type="text" id="titre" /><br />
+	<label for="description">Description </label><textarea name="description" cols="40" rows="5"></textarea><br />
+	<label for="avatars">Votre Image : </label><input type="file" name="img" id="img" /><br />
+	</fieldset>
+	<p><input type="submit" value="Upload" /></p></form>
+	</div>
+	</body>
+	</html>';
+}
