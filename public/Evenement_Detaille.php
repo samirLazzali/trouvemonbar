dd9f231bd5327f47278df71e9e01f709075c35cc
hugues.genin <hugues.genin@ensiie.fr>
2018-05-21 16:43:18 +0200

    Evenement detaillé

---
+++ public/Evenement_Detaille.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Nom de l'événement </title>
+	<metac charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="Evenement_Detaille.css">
+	<link rel="stylesheet" type="text/css" href="main.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+	
+</head>
+<body>
+<?php include "upperBar.php" ?>
+<center><h1> Votre événement</h1>
+     	    <br/><br/>
+	    	<h2>  Nom de l'événement  </h2>
+	    	<br/>
+	    	<h2>  Organisateur :  </h2>
+	    	<p> ??? </p>
+	    	<br/>
+	    	<h2>Nombre de participants : </h2>
+	    	<p> ??? </p>
+	    	<br/>
+	    	<h2> Lieu de l'événement : </h2>
+	    	<p> ??? </p>
+	    	<br/>
+	    	<h2> Prix de la participation :  </h2>
+	    	<p> ??? </p>
+	    	<br/>
+	    	<h2> Description  </h2>
+	    	<br/>
+	    	<p> <em> Exemple de description </em> </p>
+	    	<br/>
+	    	<h2> Catégorie : </h2>
+	    	<p> ??? </p>
+	    	<br/>
+	    	<h2> Type de musique : </h2>
+	    	<p> ??? </p>
+	    	<br/> <br/>
+	    	<img src="./images/Apartement.jpg" width="300" height="200" alt="Photo de l'événement">
+	    	<br/> <br/>
+	    	
+	    	
+	    	
+	     </center>
+</body>
+<end>
+  <form id="retour_accueil">
+    <a href="./Accueil.php"/>
+    <input type="button" value="Revenir à l'accueil">
+  </form>
+</end>
+</html>
