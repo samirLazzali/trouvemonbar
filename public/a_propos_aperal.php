2cc4d20af228bdb0c2827af56a561af0be64b270
Thomas Gubeno <tgubeno@gmail.com>
2018-05-21 21:53:31 +0000

    Update a_propos_aperal.php

--- public/a_propos_aperal.php
+++ public/a_propos_aperal.php
@@ -33,6 +33,13 @@ menu_aperal();
 <br />
 <br />
 <br />
+Cet ancien dicton énigmatique nous a été transmis d’antan par les dieux de l’apéro.
+A Apéral, nous nous donnons comme mission divine de transmettre le savoir et les
+bons moments ! Et comment ? Avec du saucisson ! Et de la boisson ! Et encore plus
+de saucisson !
+Chez nous l’apéritif n’est pas une manière de faire mais une manière d’être. Alors
+viendez nombreux et assoiffés, et partagez avec nous l’art de la préparation (et
+surtout de la consommation) de l’apéro !
 <h4>Aperal depuis la nuit des temps est considéré comme LA référence dans l'apéritif de compétition avec notamment 3 coupe du monde à son actif</h4>
 <img src="images/aperal.png" alt="Logo Apéral" height="500" width="500">
 </body>
