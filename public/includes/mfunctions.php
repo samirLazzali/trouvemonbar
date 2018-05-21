79d69fe26ab98ff75460c62d24f620d0ffd0edcd
Corentin Lafond <corentin.lafond@ensiie.fr>
2018-05-21 03:06:08 +0200

    Avancement du matcheur. Manque les bugs et l'affichage.

--- public/includes/mfunctions.php
+++ public/includes/mfunctions.php
@@ -1,10 +1,10 @@
 <?php
 
 
-function Age($date_naissance)
+function age($date_naissance)
 {
     $am = explode('/', $date_naissance);
-    $an = explode('/', date('d/m/Y'));
+    $an = explode('/', date('d/m/y'));
 
     if(($am[1] < $an[1]) || (($am[1] == $an[1]) && ($am[0] <= $an[0])))
         return $an[2] - $am[2];
@@ -12,102 +12,122 @@ function Age($date_naissance)
     return $an[2] - $am[2] - 1;
 }
 
-function affCompat($id_the_cat) {
-    $prodBreed = 5;
-    $prodColor = 3;
-    $prodTraits = 1;
-    $prodSizeMin = 3;
-    $prodSizeMax = 5;
-    $prodCoatMin = 4;
-    $prodCoatMax = 2;
-    $prodPattern = 2;
-    $prodWeightMin = 4;
-    $prodWeightMax = 4;
-    $prodAgeMin = 7;
-    $prodAgeMax = 4;
+function affcompat($id_the_cat) {
+    $prodbreed = 5;
+    $prodcolor = 3;
+    $prodtraits = 1;
+    $prodsizemin = 3;
+    $prodsizemax = 5;
+    $prodcoatmin = 4;
+    $prodcoatmax = 2;
+    $prodpattern = 2;
+    $prodweightmin = 4;
+    $prodweightmax = 4;
+    $prodagemin = 7;
+    $prodagemax = 4;
 
-    $dbName = getenv('DB_NAME');
-    $dbUser = getenv('DB_USER');
-    $dbPassword = getenv('DB_PASSWORD');
-    $connexion = new PDO("pgsql:host=postgres user=$dbUser dbname=$dbName password=$dbPassword");
+    $dbname = getenv('db_name'); /* la connexion n'a pas déjà été faite avant ?*/
+    $dbuser = getenv('db_user');
+    $dbpassword = getenv('db_password');
+    $connexion = new pdo("pgsql:host=postgres user=$dbuser dbname=$dbname password=$dbpassword");
 
-    $the_cat = $connexion->query("SELECT *
-                                            FROM Cats
-                                            WHERE id_cat=".$id_the_cat)->fetch(PDO::FETCH_OBJ);
-    $sexCh = $the_cat->sex;
-    $breedCh = query("SELECT breed
-                      FROM Cat_breed
-                      WHERE cat=".$id_the_cat)->fetch(PDO::FETCH_OBJ)->breed;
-    $pureRace = $the_cat->purety;
+    $the_cat = $connexion->query("select *
+                                            from cats
+                                            where id_cat=".$id_the_cat)->fetch(pdo::fetch_obj);
+    $sexch = $the_cat->sex;
+    $breedch = query("select breed
+                      from cat_breed
+                      where cat=".$id_the_cat)->fetch(pdo::fetch_obj)->breed;
+    $purerace = $the_cat->purety;
 
-    if($pureRace==1)
-        $listeChats = $connexion->query("SELECT * 
-                                          FROM Cats 
-                                          JOIN Cat_breed ON cat=id_cat 
-                                          WHERE sexe=" . $sexCh . " 
-                                            AND breed=" . $breedCh);
-    else
-        $listeChats = $connexion->query("select * 
+    if($purerace==1)
+        $listechats = $connexion->query("select * 
                                           from cats 
-                                          join cat_breed ON cat=id_cat 
-                                          where sexe=" . $sexCh);
-    $chatPot=$listeChats->fetch(PDO::FETCH_OBJ);
-    while($chatPot){
-        $chatsPot[] = $chatPot->id_cat;
+                                          join cat_breed on cat=id_cat 
+                                          where sexe=" . $sexch . " 
+                                            and breed=" . $breedch);
+    else
+        $listechats = $connexion->query("select * 
+                                          from cats 
+                                          join cat_breed on cat=id_cat 
+                                          where sexe=" . $sexch);
+    $chatpot=$listechats->fetch(pdo::fetch_obj);
+    while($chatpot){
+        $chatspot[] = $chatpot->id_cat;
         $score = 0;
-        if (!is_null($the_cat->sage_min) && $the_cat->sage_min <= Age($chatPot->birthday_cat))
-            $score += $prodAgeMin;
-        if (!is_null($the_cat->sage_max) && $the_cat->sage_max <= Age($chatPot->birthday_cat))
-            $score += $prodAgeMax;
-        if (!is_null($the_cat->scsize_min) && $the_cat->scsize_min <= $chatPot->csize)
-            $score += $prodSizeMin;
-        if (!is_null($the_cat->scsize_max) && $the_cat->scsize_max <= $chatPot->csize)
-            $score += $prodSizeMax;
-        if (!is_null($the_cat->scoat_min) && $the_cat->scoat_min <= $chatPot->coat)
-            $score += $prodCoatMin;
-        if (!is_null($the_cat->scoat_max) && $the_cat->scoat_max <= $chatPot->coat)
-            $score += $prodCoatMax;
-        if (!is_null($the_cat->sweight_min) && $the_cat->sweight_min <= $chatPot->weight)
-            $score += $prodWeightMin;
-        if (!is_null($the_cat->sweight_max) && $the_cat->sweight_max <= $chatPot->weight)
-            $score += $prodWeightMax;
-        $score += $prodBreed * $connexion->query("SELECT COUNT(*)
-                                                            FROM Cat_breed 
-                                                            JOIN Searched_breeds ON Cat_breed.breed = Searched_breeds.breed
-                                                            WHERE Searched_breeds.cat=" . $id_the_cat ."
-                                                              AND Cat_breed.cat=" . $chatPot->id_cat); /*->fetch ?? */
-        $score += $prodColor * $connexion->query("SELECT COUNT(*)
-                                                            FROM Cat_color 
-                                                            JOIN Searched_colors ON Cat_color.color = Searched_colors.color
-                                                            WHERE Searched_colors.cat=" . $id_the_cat ."
-                                                              AND Cat_color.cat=" . $chatPot->id_cat);
-        $score += $prodTraits * $connexion->query("SELECT COUNT(*)
-                                                            FROM Cat_trait 
-                                                            JOIN Searched_traits ON Cat_trait.trait = Searched_traits.trait
-                                                            WHERE Searched_traits.cat=" . $id_the_cat ."
-                                                              AND Cat_trait.cat=" . $chatPot->id_cat);
-        $score += $prodPattern * $connexion->query("SELECT COUNT(*)
-                                                            FROM Searched_pattern 
-                                                            WHERE cat=" . $id_the_cat .
-                                                            "AND pattern =" . $chatPot->pattern);
-        $scoreChatsPot[] = $score;
+        if (!is_null($the_cat->sage_min) && $the_cat->sage_min <= age($chatpot->birthday_cat))
+            $score += $prodagemin;
+        if (!is_null($the_cat->sage_max) && $the_cat->sage_max <= age($chatpot->birthday_cat))
+            $score += $prodagemax;
+        if (!is_null($the_cat->scsize_min) && $the_cat->scsize_min <= $chatpot->csize)
+            $score += $prodsizemin;
+        if (!is_null($the_cat->scsize_max) && $the_cat->scsize_max <= $chatpot->csize)
+            $score += $prodsizemax;
+        if (!is_null($the_cat->scoat_min) && $the_cat->scoat_min <= $chatpot->coat)
+            $score += $prodcoatmin;
+        if (!is_null($the_cat->scoat_max) && $the_cat->scoat_max <= $chatpot->coat)
+            $score += $prodcoatmax;
+        if (!is_null($the_cat->sweight_min) && $the_cat->sweight_min <= $chatpot->weight)
+            $score += $prodweightmin;
+        if (!is_null($the_cat->sweight_max) && $the_cat->sweight_max <= $chatpot->weight)
+            $score += $prodweightmax;
+        $score += $prodbreed * $connexion->query("select count(*)
+                                                            from cat_breed 
+                                                            join searched_breeds on cat_breed.breed = searched_breeds.breed
+                                                            where searched_breeds.cat=" . $id_the_cat ."
+                                                              and cat_breed.cat=" . $chatpot->id_cat); /*->fetch ?? */
+        $score += $prodcolor * $connexion->query("select count(*)
+                                                            from cat_color 
+                                                            join searched_colors on cat_color.color = searched_colors.color
+                                                            where searched_colors.cat=" . $id_the_cat ."
+                                                              and cat_color.cat=" . $chatpot->id_cat);
+        $score += $prodtraits * $connexion->query("select count(*)
+                                                            from cat_trait 
+                                                            join searched_traits on cat_trait.trait = searched_traits.trait
+                                                            where searched_traits.cat=" . $id_the_cat ."
+                                                              and cat_trait.cat=" . $chatpot->id_cat);
+        $score += $prodpattern * $connexion->query("select count(*)
+                                                            from searched_pattern 
+                                                            where cat=" . $id_the_cat .
+                                                            "and pattern =" . $chatpot->pattern);
+        $scorechatspot[] = $score;
     }
-    if (empty($chatsPot)) {
-        print "<h3> Malheureusement, il ne semble qu'aucun chat ne corresponde à vos attentes </h3>";
-        print "<p> Nous vous invitons à soit attendre que le chat donc vous rêvez la nuit apparaisse sur le site, soit à revoir vos critères de recherche </p>";
+    if (empty($chatspot)) {
+        print "<h3> malheureusement, il ne semble qu'aucun chat ne corresponde à vos attentes </h3>";
+        print "<p> nous vous invitons à soit attendre que le chat donc vous rêvez la nuit apparaisse sur le site, soit à revoir vos critères de recherche </p>";
     }
     else {
-        array_multisort($scoreChatsPot, $chatsPot);
-        foreach($chatPot as $elu) {
-            $num = $connexion->query("SELECT phone_number
-                                                FROM Utilisateurs
-                                                WHERE cat=" . $id_the_cat .);
+        array_multisort($scorechatspot, $chatspot);
+        print "<table>";
+        foreach($chatpot as $elu) {
+            $infoelu = $connexion->query("select phone_number, name_cat, 
+                                                from utilisateurs
+                                                natural join cats 
+                                                where cat=" . $chatpot)->fetch(pdo::fetch_obj);
+            print "<tr> <td>".$infoelu->name_cat."</td><td>".$infoelu->phone_number."</td> </tr>";
         }
-
+        print "</table>";
     }
 }
 
-
+function affmenu(){
+    $dbname = getenv('db_name'); /* la connexion n'a pas déjà été faite avant ?*/
+    $dbuser = getenv('db_user');
+    $dbpassword = getenv('db_password');
+    $connexion = new pdo("pgsql:host=postgres user=$dbuser dbname=$dbname password=$dbpassword");
+    $chatsPossédés = $connexion->query("SELECT id_cat,cat_name
+                                                  FROM cats
+                                                  NATURAL JOIN utilisateur
+                                                  WHERE id_user=".XXXXX); /* à compléter */
+    print "<script>
+            <form name="choix">
+                <select name=\"liste\" onchange=\"document.getElementById('matchables').value\">'. foreach().'
+                </select>
+           </form>'
+        </script>
+    <table style='display:none;' id='matchables'>
+    </table>"
+}
 
 
 
--- public/membres/matcher.php
+++ public/membres/matcher.php
@@ -1,23 +1,39 @@
 <?php
 
-include('includes/config.php');
-include('includes/functions.php');
-
 session_start();
 header('Content-type: text/html; charset = utf-8');
+include('includes/config.php');
 
+include('includes/functions.php');
 actualiser_session();
 
 $titre = 'Matcher';
 
 include('includes/top.php'); 
-?>
 
-		<div id="contenu">
-			<p>Le meilleur site de rencontre pour félins
-			N'hésitez pas à vous <a href="membres/inscription.php">inscrire</a>
-			</p>
-		</div>
-<?php  
+$val1 = 1;
+$val2 = 102;
+$tab[] = 1;
+$tab[] = 3;
+#affMenu();
+print "<script>
+            function affMatch() {
+                var x = document.forms[\"choix\"][\"liste\"].value;
+                document.getElementById('matcher').innerHTML = \"num\" + tab[x];
+            } 
+            var tab = <?php json_encode($tab); ?>; 
+            <<form name=\"choix\">
+                <select name=\"liste\" onchange=\"affMatch()\">
+                    <OPTION VALUE=0 >Choisir une option
+                    var i;
+                    for (i = 0; i<tab.lenght; i++) {
+                        <OPTION VALUE=i >\"chat\" + i
+                    }
+                </select>
+                <input type=\"submit\" value=\"Matcher !\">
+            </form>
+       </scipt>
+       <table id='matcher'>";
+
 include('includes/bottom.php');
 ?>
