5183419644dfcf9d685ff2b89163f6ed73ee8b2d
Paul <canat.paul@gmail.com>
2018-05-21 17:38:35 +0000

    Replace header.php

--- includes/header.php
+++ includes/header.php
@@ -7,16 +7,17 @@ include("includes/debut.php");?>
 <html lang="fr">
 <body>
 <h1 id="title">
-    <a href="index.php"><img class="home" src="img/logo.png" alt="Logo" height ="88" width="94"></a>
-    <span id="title1">GolrIIE</span>
-    <a href="index.php"><img class="dc" src="img/logo.png" alt="Logo" height ="88" width="94"></a>
+    <div class="cadre">
+        <a href="index.php"><img class="home" src="img/logo.png" alt="Logo" height ="88" width="94"></a>
+        <span id="title1">GolrIIE</span>
+    </div>
 </h1>
 <?php
     if ($id!=0) {
         include("./includes/menuderoulant.php");
     }
-    else{
-            include("./includes/menuderoulant2.php");
+    else[
+            include("./includes/menuderoulant2.php"};
     }
 ?>
 </body>
