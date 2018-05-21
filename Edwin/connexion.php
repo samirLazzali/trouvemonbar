e2aa1fc0b231ce474d6e2f97872199c0d71fd92c
Mauillon <edwin.mauillon@wanadoo.fr>
2018-05-21 08:24:23 +0000

    Update connexion.php

--- Edwin/connexion.php
+++ Edwin/connexion.php
@@ -1,4 +1,5 @@
 <?php
+require '../vendor/autoload.php';
 // on teste si le visiteur a soumis le formulaire de connexion
 if (isset($_POST['connexion']) && $_POST['connexion'] == 'Connexion') {
     if ((isset($_POST['login']) && !empty($_POST['login'])) && (isset($_POST['password']) && !empty($_POST['password']))) {
