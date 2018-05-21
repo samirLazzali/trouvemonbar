e72404e5f20176e20ad1bea29fd2ac666e90364c
Thomas GUBENO <thomas.gubeno@ensiie.fr>
2018-05-22 01:30:58 +0200

    index

--- public/index.php
+++ public/index.php
@@ -47,11 +47,10 @@ menu_navigation();
 <br />
 <br />
 <br />
-<h1>Nique les pd.</h1>
                                         
 
 
-<h3>Prochaine réunion</h3>
+<h2>Prochaine réunion</h2>
     <?php
     $req_count = $connection->query('SELECT COUNT(*) AS nbr FROM public.reunion')->fetch();
     if ($req_count['nbr']!=0) {
@@ -85,7 +84,7 @@ menu_navigation();
         }
     }
     else{
-         echo "pas de réunion plannifiée";
+         echo "Aucune réunion planifiée";
     }
     ?>
 </body>
