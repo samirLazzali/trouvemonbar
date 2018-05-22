56db9bbcb68ace0ecb3e3b45564de0ab876e9106
Corentin Lafond <corentin.lafond@ensiie.fr>
2018-05-22 06:47:27 +0200

    le topbot qui tente de se barrer

--- public/includes/topbot.php
+++ public/includes/topbot.php
@@ -10,14 +10,15 @@
 			$dbPassword = getenv('DB_PASSWORD');
 				
 			$connexion = new PDO("pgsql:host=postgres user=$dbUser dbname=$dbName password=$dbPassword");
-			$infos = $connexion -> query("SELECT user_type FROM Utilisateur WHERE id_user = ".intval($_SESSION['id_user']));
-			$infos -> setFetchMode(PDO::FETCH_OBJ);
-			$fetch = $infos -> fetch();
+
 				
 			global $queries;
 			$queries++;
 	 if(isset($_SESSION['id_user']))
 	 {
+         $infos = $connexion -> query("SELECT user_type FROM Utilisateur WHERE id_user = ".intval($_SESSION['id_user']));
+         $infos -> setFetchMode(PDO::FETCH_OBJ);
+         $fetch = $infos -> fetch();
 	 ?>
       <a href="<?php echo ROOTPATH; ?>/index.php">Accueil</a>   &nbsp;<a href="<?php echo ROOTPATH; ?>/membres/moncompte.php">Gérer mon compte</a>   &nbsp;<a href="<?php echo ROOTPATH; ?>/cats/search_cat.php">Mes recherches</a>   &nbsp;<a href="<?php echo ROOTPATH; ?>/membres/matcher.php">Matcher mes chats</a>   &nbsp;<a href="<?php echo ROOTPATH; ?>/membres/deconnexion.php">Se déconnecter</a>
       <?php if($fetch->user_type == 0) { echo ROOTPATH; ?>/admin/crud.php>Administration</a>   &nbsp; <?php
