96deac1d831cd57e9bd7eed15f23ec14d5af8bd0
Corentin Lafond <corentin.lafond@ensiie.fr>
2018-05-22 07:44:15 +0200

    ajout du logo

--- public/includes/toptop.php
+++ public/includes/toptop.php
@@ -13,4 +13,5 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta name="language" content="fr" />
     <link rel="stylesheet" type="text/css" href="../style.css"/>
+    <link rel="icon" type="image/png" href="/img/favicon.ico">
 
