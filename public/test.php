1cad23f343068a6b600ba5cf33faab556d71ee58
Joséphine Barthel <josephine.barthel11@gmail.com>
2018-05-22 10:39:05 +0200

    new db + corrected bootstrap include

---
+++ public/test.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jo
+ * Date: 22/05/18
+ * Time: 09:50
+ */
--- src/app/views/_layouts/visitors.php
+++ src/app/views/_layouts/visitors.php
@@ -4,7 +4,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta charset="UTF-8">
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/calendar.css">
 
     <title> <?=$title ?>  </title>
 </head>
@@ -26,9 +25,10 @@
     <?= $this->contents ?>
 </div>
 
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<link rel="script" href="js/jquery-3.3.1.min.js">
+<link rel="script" href="js/bootstrap.bundle.min.js">
+<link rel="script" href="js/bootstrap.min.js">
+
 </body>
 
 
