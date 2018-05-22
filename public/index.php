133553c7183a443fcb2148a8ada6b5d4a5f3362f
Psiich0 <thomas.beratto@ensiie.fr>
2018-05-22 06:07:55 +0200

    gfgrg

--- public/index.php
+++ public/index.php
@@ -13,34 +13,13 @@ $titre = '';
 include('includes/top.php'); 
 ?>
 		<?php
-		if(isset($_SESSION['id_user']))
-			{ ?>
-				<div id="contenu">
-					<?php include('includes/connected_index.php'); ?>
-				</div>
-				</p>
-				<?php
-			}
-		
-		else { ?>
-		<div id="contenu">
-			<p>Depuis 2018, la communauté <strong>CATisfaction</strong> vise à mettre en relation les différents amoureux des chats afin que leur compagnions
-			préférés puisse eux aussi avoir droit à l'amour. Vous en avez marre des chaleurs de votre chatte, ou bien encore vous souhaitez que votre
-			matou adoré vive lui aussi une grande romance, ce site est fait pour vous. Grâce à notre grande communauté d'utilisateurs, vous trouverez ici
-			le partenaire idéal de votre félin et selon vos préférences grâce à notre système de recherche.<br/>
-			
-			<strong>CATisfaction</strong> : Le meilleur site de rencontre pour félins
-			N'hésitez pas à vous <a href="membres/inscription.php">inscrire</a>
-			</p>
-		</div><?php  
+		if(isset($_SESSION['id_user'])) {
+			include('includes/connected_index.php');
 		}
-if(isset($_SESSION['id_user'])) {
-    include('includes/connected_index.php');
-}
-else {
-    include('includes/top.php'); ?>
-    <div id="contenu">
-        <p>
+		else {
+			include('includes/top.php'); ?>
+			<div id="contenu">
+			<p>
             Depuis 2018, la communauté CATisfaction vise à mettre en relation les différents amoureux des chats afin que leur compagnions
             préférés puisse eux aussi avoir droit à l'amour. Vous en avez marre des chaleurs de votre chatte, ou bien encore vous souhaitez que votre
             matou adoré vive lui aussi une grande romance, ce site est fait pour vous. Grâce à notre grande communauté d'utilisateurs, vous trouverez ici
@@ -48,9 +27,10 @@ else {
 
             CATisfaction : Le meilleur site de rencontre pour félins
             N'hésitez pas à vous <a href="membres/inscription.php">inscrire</a>
-        </p>
-    </div><?php
-}
+			</p>
+			</div><?php
+		}
+
 
 include('includes/bottom.php');
 ?>
