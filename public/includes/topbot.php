e9ff63d59b62ff1d3d4f27d79c0e4d5bbd5b4ede
Psiich0 <thomas.beratto@ensiie.fr>
2018-05-22 07:41:52 +0200

    PASTOUCHE

--- public/includes/topbot.php
+++ public/includes/topbot.php
@@ -4,21 +4,18 @@
       CATISFACTION
     </div>
     <div id="menu">
-			<?php
-			$dbName = getenv('DB_NAME');
-			$dbUser = getenv('DB_USER');
-			$dbPassword = getenv('DB_PASSWORD');
-				
-			$connexion = new PDO("pgsql:host=postgres user=$dbUser dbname=$dbName password=$dbPassword");
-
-				
-			global $queries;
-			$queries++;
+	<?php	
 	 if(isset($_SESSION['id_user']))
 	 {
-         $infos = $connexion -> query("SELECT user_type FROM Utilisateur WHERE id_user = ".intval($_SESSION['id_user']));
-         $infos -> setFetchMode(PDO::FETCH_OBJ);
-         $fetch = $infos -> fetch();
+		$dbName = getenv('DB_NAME');
+		$dbUser = getenv('DB_USER');
+		$dbPassword = getenv('DB_PASSWORD');
+		$connexion = new PDO("pgsql:host=postgres user=$dbUser dbname=$dbName password=$dbPassword");
+		global $queries;
+		$queries++;
+		$infos = $connexion -> query("SELECT user_type FROM Utilisateur WHERE id_user = ".intval($_SESSION['id_user']));
+        $infos -> setFetchMode(PDO::FETCH_OBJ);
+		$fetch = $infos -> fetch();
 	 ?>
       <a href="<?php echo ROOTPATH; ?>/index.php">Accueil</a>   &nbsp;<a href="<?php echo ROOTPATH; ?>/membres/moncompte.php">Gérer mon compte</a>   &nbsp;<a href="<?php echo ROOTPATH; ?>/cats/search_cat.php">Mes recherches</a>   &nbsp;<a href="<?php echo ROOTPATH; ?>/membres/matcher.php">Matcher mes chats</a>   &nbsp;<a href="<?php echo ROOTPATH; ?>/membres/deconnexion.php">Se déconnecter</a>
       <?php if($fetch->user_type == 0) { echo ROOTPATH; ?>/admin/crud.php>Administration</a>   &nbsp; <?php
