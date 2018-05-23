5e24dd15f9d8661e066445a8b7f1abef685692c0
PEREIRA Romain <romain.pereira@ensiie.fr>
2018-05-23 11:57:19 +0200

    Update index.php
    nettoyage du code/commentaire

--- public/index.php
+++ public/index.php
@@ -9,19 +9,13 @@ use Controller\Navbar;
 use Controller\Sidebar;
 use Controller\Toastbar;
 use Model\ULC\BDD\BDD;
-use Model\ULC\Permissions\Permission;
 use Model\ULC\Utilisateur\Utilisateur;
 
 require ('../vendor/autoload.php');
 define ( 'PROJECT_ROOT', realpath ( dirname ( __FILE__ ) . "/../" ) );
 define ( 'VIEW_FOLDER', PROJECT_ROOT . "/src/View" );
 
-/* recupere la session */
-session_start ();
-
-/**
- * DEBUT : AFFICHAGE DE LA PAGE
- */
+/* les éléments de la page (afficher dans l'ordre naturel du tableau) */
 $pageElementsClass = [ 
 		Header::class,
 		Toastbar::class,
@@ -32,16 +26,15 @@ $pageElementsClass = [
 		Footer::class 
 ];
 
-/* recupere l'utilisateur */
+/* recupere la base de données */
 $bdd = BDD::instance ();
 
 /* recupere l'utilisateur */
 $user = Utilisateur::instance ();
 
+/* instancie puis affiche chaque element de la page */
 foreach ( $pageElementsClass as $pageElementClass ) {
 	$pageElement = new $pageElementClass ( $bdd, $user );
 	$pageElement->afficher ();
 }
 
-
-/** FIN : AFFICHAGE DE LA PAGE */
