dd4b44d8d173f57db4f1ecec0584e6d7d623d472
Swann CASTEL <swann.castel@hotmail.fr>
2018-10-20 00:12:43 +0200

    refacto: use userRepository in router

--- back/public/index.php
+++ back/public/index.php
@@ -11,15 +11,50 @@ $connection = new PDO("pgsql:host=postgres user=$dbUser dbname=$dbName password=
 
 $userHydrator = new \User\UserHydrator();
 $userRepository = new \User\UserRepository($connection, $userHydrator);
-$users = $userRepository->fetchAll();
 
-$data = [];
-foreach ($users as $user) {
-    $data[] = $userHydrator->extract($user);
-}
+// get all users
+Router::get('/api/users', function() use($userRepository, $userHydrator) {
+    $users = $userRepository->fetchAll();
 
-Router::get('/api/users', function() use($data) {
+    $data = [];
+    foreach ($users as $user) {
+        $data[] = $userHydrator->extract($user);
+    }
+
     echo json_encode($data);
 });
 
 Router::execute();
+
+// simple route
+
+// get the user 1
+// Router::get('/api/users/1, function() {});
+
+// create a new user with a request body
+// Router::post('/api/users', function() {});
+
+// update the user 1 with a request body
+// Router::put('/api/users/1', function() {});
+
+// delete all users
+// Router::delete('/api/users', function() {});
+
+// delete the user 1
+// Router::delete('/api/users/1', function() {});
+
+// for create and delete, if id 1 does not exist return 404
+
+// nested route
+
+// get all messages of the user 1
+// Router::get('/api/users/1/messages, function() {})
+
+// create a message of the user 1 with the request body
+// Router::post('/api/users/1/messages, function() {})
+
+// update the message 2 of the user 1 with the request body
+// Router::put('/api/users/1/messages/2, function() {})
+
+// delete the message 2 of the user 1
+// Router::delete('/api/users/1/messages/2, function() {})
