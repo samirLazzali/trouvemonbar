4d6443a64f56575c8259e788ae8e2d30d9773a06
Swann CASTEL <swann.castel@hotmail.fr>
2018-10-19 21:40:46 +0200

    refacto: use static router in test

--- back/spec/Router/Router.spec.php
+++ back/spec/Router/Router.spec.php
@@ -2,30 +2,26 @@
 namespace Router;
 
 describe('Router', function() {
-    beforeEach(function() {
-        $this->router = new Router();
-    });
-
     it('should handle get request', function() {
         $_SERVER['REQUEST_METHOD'] = 'GET';
         $_SERVER['REQUEST_URI'] = '/test/awesome/get';
 
-        $this->router::get('/test/{}/{}', function($awesome, $get) {
+        Router::get('/test/{}/{}', function($awesome, $get) {
             return [$awesome, $get];
         });
 
-        expect($this->router::execute())->toBe(['awesome', 'get']);
+        expect(Router::execute())->toBe(['awesome', 'get']);
     });
 
     it('should handle post request', function() {
         $_SERVER['REQUEST_METHOD'] = 'POST';
         $_SERVER['REQUEST_URI'] = '/test/awesome/post';
 
-        $this->router::post('/test/{}/{}', function($awesome, $post) {
+        Router::post('/test/{}/{}', function($awesome, $post) {
             return [$awesome, $post];
         });
 
-        expect($this->router::execute())->toBe(['awesome', 'post']);
+        expect(Router::execute())->toBe(['awesome', 'post']);
     });
 
 
@@ -33,10 +29,10 @@ describe('Router', function() {
         $_SERVER['REQUEST_METHOD'] = 'DELETE';
         $_SERVER['REQUEST_URI'] = '/test/awesome/delete';
 
-        $this->router::delete('/test/{}/{}', function($awesome, $delete) {
+        Router::delete('/test/{}/{}', function($awesome, $delete) {
             return [$awesome, $delete];
         });
 
-        expect($this->router::execute())->toBe(['awesome', 'delete']);
+        expect(Router::execute())->toBe(['awesome', 'delete']);
     });
 });
