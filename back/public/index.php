b89047774266421e6351616d427030a5c48f77a6
Jérôme O'keeffe <okeeffe.jerome@outlook.com>
2018-10-21 18:29:23 +0200

    fix

--- back/public/index.php
+++ back/public/index.php
@@ -41,7 +41,8 @@ Router::get('/api/bars/{}', function($request) use($barRepository, $barHydrator)
     if($id != '' and is_int($id))
 	{
     	$bar = $barRepository->fetchById($id);
-    	echo json_encode($barHydrator->extract($bar));
+    	print_r($bar);
+    	// echo json_encode($barHydrator->extract($bar));
 	}
 	else
 	{
