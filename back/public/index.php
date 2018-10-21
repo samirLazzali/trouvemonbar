4c667d33c07ea93a0d37e4324ab77c868ba8385d
Jérôme O'keeffe <okeeffe.jerome@outlook.com>
2018-10-21 18:32:50 +0200

    Update back/public/index.php

--- back/public/index.php
+++ back/public/index.php
@@ -41,8 +41,7 @@ Router::get('/api/bars/{}', function($request) use($barRepository, $barHydrator)
     if($id != '' and is_int($id))
 	{
     	$bar = $barRepository->fetchById($id);
-    	echo($bar->getId());
-    	// echo json_encode($barHydrator->extract($bar));
+    	echo json_encode($barHydrator->extract($bar));
 	}
 	else
 	{
