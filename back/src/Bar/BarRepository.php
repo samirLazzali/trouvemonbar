f3326260af6423fe412fd2e4fe3f884bc54bd9ca
Jérôme O'keeffe <okeeffe.jerome@outlook.com>
2018-10-21 20:37:33 +0200

    Update back/src/Bar/BarRepository.php

--- back/src/Bar/BarRepository.php
+++ back/src/Bar/BarRepository.php
@@ -26,7 +26,7 @@ class BarRepository
         // Return False if an error occured
         $request = $this->connection->prepare('SELECT * FROM "bar" where id=:id');
         $request = $request->bindValue(':id',$id, \PDO::PARAM_INT);
-        $request->setFetchMode(\PDO::FETCH_CLASS, Bar::class);
+        $request->setFetchMode(\PDO::FETCH_CLASS, 'Bar');
         $request->execute();
         return $request->fetch();
     }
