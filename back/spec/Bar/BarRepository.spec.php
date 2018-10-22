edcfa1de242dec880585098267de295dc694c6dc
Jérôme O'keeffe <okeeffe.jerome@outlook.com>
2018-10-22 20:36:51 +0200

    Update back/spec/Bar/BarRepository.spec.php

--- back/spec/Bar/BarRepository.spec.php
+++ back/spec/Bar/BarRepository.spec.php
@@ -12,7 +12,7 @@ describe('BarRepository', function() {
         expect($bars[0]->getId())->toBe(1);
         expect($bars[0]->getName())->toBe('Un bar à Evry');
         expect($bars[0]->getAddress())->toBe('3 rue André Lalande');
-        expect($bars[0]->getKeywords())->toBe(['AAA']);
+        // expect($bars[0]->getKeywords())->toBe(['AAA']);
 
         expect($bars[1]->getId())->toBe(2);
         expect($bars[1]->getName())->toBe('Un bar à Grigny');
@@ -29,8 +29,8 @@ class MockPDO {
             (new Bar())
             ->setId(1)
             ->setName('Un bar à Evry')
-            ->setAddress('3 rue André Lalande')
-            ->addKeyword('AAA'),
+            ->setAddress('3 rue André Lalande'),
+            // ->addKeyword('AAA'),
             (new Bar())
             ->setId(2)
             ->setName('Un bar à Grigny')
