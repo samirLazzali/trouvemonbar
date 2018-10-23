72b1c860279cb98c2fd64e9a9183c01e628e8cd3
Swann CASTEL <swann.castel@hotmail.fr>
2018-10-23 16:49:55 +0200

    feat: update the query search

--- front/src/views/TheSearch.vue
+++ front/src/views/TheSearch.vue
@@ -2,9 +2,19 @@
   <div>
     <search-bar
       :keywords="keywords"
+      v-model="selectedKeywords"
+      @input.once="changed = true"
+      @search="search"
     ></search-bar>
 
     <v-container fluid grid-list-xl>
+      <v-progress-linear
+        v-if="loading"
+        color="success"
+        height="4"
+        indeterminate
+      ></v-progress-linear>
+
       <v-layout row wrap>
         <bar
           v-for="{ id, name, address } in bars"
@@ -39,22 +49,44 @@ export default {
 
   data () {
     return {
-      bars: []
-    }
-  },
-
-  computed: {
-    keywords () {
-      return []
+      keywords: [],
+      selectedKeywords: [],
+      bars: [],
+      loading: true
     }
   },
 
   created () {
-    this.$log.debug(this.query)
-
-    this.$api.searchRequest(this.query)
-      .then(bars => (this.bars = bars))
+    this.$api.getKeywords()
+      .then(keywords => (this.keywords = keywords))
       .catch(this.$log.error)
+  },
+
+  watch: {
+    query: {
+      immediate: true,
+      handler () {
+        this.$log.debug(this.query)
+
+        this.selectedKeywords = this.query.q.split(',')
+
+        this.$api.searchRequest(this.query)
+          .then(bars => (this.bars = bars))
+          .catch(this.$log.error)
+          .finally(() => (this.loading = false))
+      }
+    }
+  },
+
+  methods: {
+    search () {
+      if (this.selectedKeywords.join(',') === this.query.q) return
+      if (this.selectedKeywords.length === 0) return
+
+      this.bars = []
+      this.loading = true
+      this.$router.push(`/search?q=${this.selectedKeywords.join(',')}`)
+    }
   }
 }
 </script>
