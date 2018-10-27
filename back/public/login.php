435d4deeb33a27aa83dda82644b5deab17ccf0ec
Swann CASTEL <swann.castel@hotmail.fr>
2018-10-27 17:51:49 +0200

    feat(login.php): check user and give token

--- back/public/login.php
+++ back/public/login.php
@@ -8,22 +8,18 @@ $userHydrator = new \User\UserHydrator();
 $userRepository = new \User\UserRepository($connection, $userHydrator);
 
 Router::post('/api/login', function($request) use($userRepository, $userHydrator) {
+    if (is_null($request->body)) return http_response_code(400);
+
     $email = $request->body->email;
     $hash = hash('sha256', $request->body->password);
     $user = $userRepository->fetchByEmailAndHash($email, $hash);
 
-    if ($user) {
-        $id = $user->getId();
-        $header = '{"alg":"HS256","typ":"JWT"}';
-        $payload = '{"id":"1"}';
+    if (!$user) return http_response_code(401);
 
-        $base64 = base64_encode($header) . '.' . base64_encode($payload);
-        echo $payload;
-        echo $base64;
+    $id = $user->getId();
+    $secret = 'secret123';
 
-        $signature = hash_hmac('sha256', $base64 , 'secret');
-        echo $base64 . '.' . $signature;
-    } else {
-        http_response_code(401);
-    }
+    $token = \Token\JwtHS256::generate($id, $secret, time());
+
+    header("Authorization: Bearer $token");
 });
