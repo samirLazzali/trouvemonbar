b99358169cce0d1d1d09e9b10d6f539291a8880c
Swann CASTEL <swann.castel@hotmail.fr>
2018-10-28 15:39:19 +0100

    feat: add a store

---
+++ front/src/store.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    user: null
+  },
+  mutations: {
+    user (state, user) {
+      state.user = user
+    }
+  },
+  actions: {
+    logout ({ commit }) {
+      commit('user', null)
+      localStorage.removeItem('user-toker')
+    }
+  }
+})
