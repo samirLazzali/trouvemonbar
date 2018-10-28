f4980449fe4b1c7847f754df160a08f9ca3c0479
Swann CASTEL <swann.castel@hotmail.fr>
2018-10-28 16:16:16 +0100

    refacto: center align the search bar

--- front/src/views/TheHome.vue
+++ front/src/views/TheHome.vue
@@ -1,22 +1,26 @@
 <template>
-  <div>
-    <v-card color="transparent" flat>
-        <v-card-text class="light-green--text">
-          <h1 class="text-xs-center">La flemme de chercher un bar ?</h1>
-          <h1 class="text-xs-center">On s'occupe de tout !</h1>
-        </v-card-text>
-    </v-card>
+  <v-content>
+    <v-container fluid>
+      <v-layout align-center justify-center row wrap>
+        <v-flex xs12>
+          <v-card color="transparent" flat>
+            <v-card-text class="light-green--text">
+              <h1 class="text-xs-center">La flemme de chercher un bar ?</h1>
+              <h1 class="text-xs-center">On s'occupe de tout !</h1>
+            </v-card-text>
+          </v-card>
+        </v-flex>
 
-    <v-layout row wrap>
-      <v-flex xs12 lg8 xl6 offset-lg2 offset-xl3>
-        <search-bar
-          :keywords="keywords"
-          v-model="selectedKeywords"
-          @search="search"
-        ></search-bar>
-      </v-flex>
-    </v-layout>
-  </div>
+          <v-flex xs12 lg8 xl6>
+            <search-bar
+              :keywords="keywords"
+              v-model="selectedKeywords"
+              @search="search"
+            ></search-bar>
+          </v-flex>
+      </v-layout>
+    </v-container>
+  </v-content>
 </template>
 
 <script>
