7130bd6ae18e9c93cff56b981e4b65449fef759e
emilio <desousa.emilio.info@gmail.com>
2018-10-29 10:56:46 +0100

    indent

--- front/src/views/TheBar.vue
+++ front/src/views/TheBar.vue
@@ -2,62 +2,58 @@
 <template>
 <div>
   <v-flex xs12>
-      <v-parallax height="400" :src="require('@/assets/img/bg7.jpg')">
-   <v-layout
+    <v-parallax height="400" :src="require('@/assets/img/bg7.jpg')">
+      <v-layout
       align-center
       column
       justify-center
-    >
-
+      >
       <h1 class="display-3 font-weight-thin mb-5 pb-5 ">{{bar.name}}</h1>
-    </v-layout>
-  </v-parallax>
-            <v-card color="margin_test" class=" ml-5 mr-5 pt-5 mb-5 elevation-23" >
-              <v-layout>
-                <v-flex xs4>
-                  <v-img
-                    src="https://lh3.googleusercontent.com/p/AF1QipOQ3rYB5ZLuGaQZ7ic1nzcetIR24wF5diRTaaaJ=s1600-w400"
-                    height="200px"
-                    contain
-                   ></v-img>
-                </v-flex>
-                <v-flex xs7>
-                  <v-card-title primary-title>
-                     <v-card-actions class="display-1">
-                <v-spacer></v-spacer>
-              </v-card-actions>
-              </v-card-actions>
-                <div>
-                  <div class="headline">Mots clefs:</div>
-                    <v-chip
-                    v-for="keyword in bar.keywords"
-                    :key="keyword"
-                    outline color="green darken-1"
-                    >
-                    {{ keyword }}
-                    </v-chip>
-                </div>
-                </v-card-title>
-                </v-flex>
-              </v-layout>
-              <v-divider class="mt-5 elevation-2 " light ></v-divider>
-              <v-card color="" class=" pb-5">
-                <v-card-title primary-title>
-                  <div class="display-1">Description</div>
-                </v-card-title>
-
-                <v-card-text>
-                  <div class="subheading">
-                    Nom : {{bar.name }}
-                    </br>
-                    Addresse : {{bar.address }}
-                  </div>
-                </v-card-text>
-            </v-card>
-
-
-            </v-card>
+      </v-layout>
+    </v-parallax>
+      <v-card color="margin_test" class=" ml-5 mr-5 pt-5 mb-5 elevation-23" >
+        <v-layout>
+          <v-flex xs4>
+            <v-img
+              src="https://lh3.googleusercontent.com/p/AF1QipOQ3rYB5ZLuGaQZ7ic1nzcetIR24wF5diRTaaaJ=s1600-w400"
+              height="200px"
+              contain
+              ></v-img>
           </v-flex>
+          <v-flex xs7>
+            <v-card-title primary-title>
+              <v-card-actions class="display-1">
+              <v-spacer></v-spacer>
+              </v-card-actions>
+
+            <div class="headline">Mots clefs:</div>
+              <v-chip
+              v-for="keyword in bar.keywords"
+              :key="keyword"
+              outline color="green darken-1"
+              >
+              {{ keyword }}
+              </v-chip>
+
+          </v-card-title>
+          </v-flex>
+        </v-layout>
+        <v-divider class="mt-5 elevation-2 " light ></v-divider>
+        <v-card color="" class=" pb-5">
+          <v-card-title primary-title>
+            <div class="display-1">Description</div>
+          </v-card-title>
+
+          <v-card-text>
+            <div class="subheading">
+              Nom : {{bar.name }}
+              <br>
+              Addresse : {{bar.address }}
+            </div>
+          </v-card-text>
+         </v-card>
+        </v-card>
+    </v-flex>
   </div>
 </template>
 
