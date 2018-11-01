e925c4b4507e0b320d74d3127eb968cb769dcac6
Swann CASTEL <swann.castel@hotmail.fr>
2018-11-01 14:58:45 +0100

    refacto(User): add isValidUser fonction

--- back/public/user.php
+++ back/public/user.php
@@ -5,6 +5,7 @@ use \Token\JwtHS256;
 
 $pdo = \Database\DatabaseSingleton::getInstance();
 $userHydrator = new \User\UserHydrator();
+$userValidator = new \User\UserValidator();
 $userRepository = new \User\UserRepository($pdo, $userHydrator);
 
 Router::get('/api/users/{}', function() use($userRepository, $userHydrator) {
@@ -26,47 +27,31 @@ Router::get('/api/users/{}', function() use($userRepository, $userHydrator) {
     }
 });
 
-Router::post('/api/users', function($request) use($userRepository, $userHydrator) {
-    if (is_null($request->body) || !(isset($request->body->email)  && isset($request->body->pseudo) && isset($request->body->password))) return http_response_code(400);
-
-    // Get all the information from the body
-    $email = $request->body->email;
-    $pseudo = $request->body->pseudo;
-    $password = $request->body->password;
-
-    // Check the pattern of the email
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+Router::post('/api/users', function($request) use($userRepository, $userValidator) {
+    if (!$userValidator->validate($request->body)) {
         return http_response_code(400);
     }
 
-    if(strlen($password) < 3 || strlen($pseudo) < 3) return http_response_code(400);
+    $user = (new \User\User())
+        ->setEmail($request->body->email)
+        ->setHash(hash('sha256', $request->body->password))
+        ->setPseudo($request->body->pseudo)
+        ->setRole('USER');
 
-    $hash = hash('sha256', $password);
-
-    // Checking whether or not a account already exist with the same email or pseudo
-    if($userRepository->pseudoChecker($pseudo) || $userRepository->emailChecker($email) ){
-        return http_response_code(418);
+    if(!$userRepository->isValidUser($user)) {
+        http_response_code(400);
+        echo json_encode(['error' => 'Email or pseudo already exist']);
+        return;
     }
 
-    $user = new \User\User();
-    $user->setEmail($email);
-    $user->setHash($hash);
-    $user->setPseudo($pseudo);
-    $user->setRole('USER');
-
-    if(!$userRepository->signupUser($user))
-    {
+    if(!$userRepository->createUser($user)) {
         return http_response_code(500);
+    } else {
+        return http_response_code(201);
     }
-    else
-    {
-        return http_response_code(200);
-    }
-
 });
 
 Router::put('/api/users/{}', function($request) use($userRepository, $userHydrator) {
-    // Get the token, check validity and if valid get the user
     if (!array_key_exists('HTTP_AUTHORIZATION', $_SERVER)) {
         http_response_code(401);
         echo json_encode(['error' => 'You are not authorized without JWT']);
@@ -80,10 +65,9 @@ Router::put('/api/users/{}', function($request) use($userRepository, $userHydrat
         $user = $userRepository->fetchById($userId);
     } catch (Exception $e) {
         http_response_code(401);
+        echo json_encode(['error' => $e->getMessage()]);
         return;
     }
-
-
 
     // Check parameter
     if (is_null($request->body) || !(isset($request->params[0]) && isset($request->body->user->password))) return http_response_code(400);
