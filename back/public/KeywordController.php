4c2880dcaa8dc3aa0d80cdfc80bc01b9280da5be
Swann CASTEL <swann.castel@hotmail.fr>
2018-11-10 21:32:06 +0100

    refacto(KeywordController): remove useless things

--- back/public/KeywordController.php
+++ back/public/KeywordController.php
@@ -1,6 +1,5 @@
 <?php
 
-
 use \Router\Router;
 
 $pdo =\Database\DatabaseSingleton::getInstance();
@@ -9,10 +8,10 @@ $userRepository = new \User\UserRepository($pdo);
 $keywordHydrator = new \Keyword\KeywordHydrator();
 $barHydrator = new \Bar\BarHydrator();
 
-
 Router::get('/api/keywords', function() use($keywordRepository, $keywordHydrator) {
-    // http://localhost:3000/api/addbar?keywords=%22o%22
     $keywords = $keywordRepository->fetchAll();
+
     if (!$keywords) return http_response_code(500);
+
     echo json_encode($keywordHydrator->extractAll($keywords), JSON_UNESCAPED_UNICODE);
 });
