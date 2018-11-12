6dde820c876dfe3c3f0e5c1ed955ce918aa9674f
Swann CASTEL <swann.castel@hotmail.fr>
2018-11-12 17:31:58 +0100

    test(App.vue): add test for app

---
+++ front/tests/unit/App/App.spec.js
@@ -0,0 +1,34 @@
+import { shallowMount } from '@vue/test-utils'
+import App from '@/App'
+
+describe('App.vue', () => {
+  it('should render properly when not authenticated', () => {
+    const wrapper = shallowMount(App, {
+      mocks: {
+        $store: {
+          getters: {
+            isAuthenticated: false
+          }
+        }
+      },
+      stubs: ['router-link', 'router-view']
+    })
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('should render properly when authenticated', () => {
+    const wrapper = shallowMount(App, {
+      mocks: {
+        $store: {
+          getters: {
+            isAuthenticated: true
+          }
+        }
+      },
+      stubs: ['router-link', 'router-view']
+    })
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+})
