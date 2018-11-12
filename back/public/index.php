9a37df3bed686bf2c07b589aa7b9a288407c2b61
Samir Lazzali <samir.lazzali@gmail.com>
2018-11-12 18:04:52 +0100

    Remove bar from index.php

--- back/public/index.php
+++ back/public/index.php
@@ -5,68 +5,6 @@ require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/KeywordController.php';
 require_once __DIR__ . '/LoginController.php';
 require_once __DIR__ . '/UserController.php';
-require_once __DIR__ . '/SearchController.php';
 require_once __DIR__ . '/BarController.php';
 
-
-use \Router\Router;
-
-$pdo = \Database\DatabaseSingleton::getInstance();
-$barHydrator = new \Bar\BarHydrator();
-$barRepository = new \Bar\BarRepository($pdo, $barHydrator);
-
-Router::get('/api/bars\?keywords\=(.+)', function($request) use($barRepository, $barHydrator)
-{
-    $keywords = explode(',', rawurldecode($request->params[0]));
-    $bars = $barRepository->fetchByKeyWords($keywords);
-
-    if($bars == NULL) {
-        http_response_code(404);
-        echo json_encode(array('error' => 'No such bar with those keywords'));
-        return;
-    }
-
-    echo json_encode($barHydrator->extractAll($bars), JSON_UNESCAPED_UNICODE);
-});
-
-Router::get('/api/bars/{}', function($request) use($barRepository, $barHydrator)
-{
-    $id = intval($request->params[0]);
-
-    if (strval($id) !== $request->params[0]) {
-    	http_response_code(400);
-        echo json_encode(array('error' => 'Parameters are not correct.'));
-        return;
-    }
-
-    $bar = $barRepository->fetchById($id);
-    if ($bar == null) {
-        http_response_code(404);
-        echo json_encode(array('error' => 'No such bar with this id.'));
-        return;
-    }
-
-    echo json_encode($barHydrator->extract($bar), JSON_UNESCAPED_UNICODE);
-});
-
-Router::get('/api/bars/{}', function($request) use($barRepository, $barHydrator)
-    {
-        $id = intval($request->params[0]);
-
-        if (strval($id) !== $request->params[0]) {
-            http_response_code(400);
-            echo json_encode(array('error' => 'Parameters are not correct.'));
-            return;
-        }
-
-        $bar = $barRepository->fetchById($id);
-        if ($bar == null) {
-            http_response_code(404);
-            echo json_encode(array('error' => 'No such bar with this id.'));
-            return;
-        }
-
-        echo json_encode($barHydrator->extract($bar), JSON_UNESCAPED_UNICODE);
-    });
-
-Router::execute();
+\Router\Router::execute();
