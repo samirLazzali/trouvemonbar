854c3c099a47dc276e9bc2def79f886578ea99c2
Samir Lazzali <samir.lazzali@gmail.com>
2018-11-12 18:05:42 +0100

    Add search controller

--- back/public/index.php
+++ back/public/index.php
@@ -6,5 +6,7 @@ require_once __DIR__ . '/KeywordController.php';
 require_once __DIR__ . '/LoginController.php';
 require_once __DIR__ . '/UserController.php';
 require_once __DIR__ . '/BarController.php';
+require_once __DIR__ . '/SearchController.php';
+
 
 \Router\Router::execute();
