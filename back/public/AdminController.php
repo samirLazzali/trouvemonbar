44d84d85b40d4d63421bb49543ef986b6f2886f2
jayok <jerome.okeeffe@ensiie.fr>
2018-11-12 22:10:22 +0100

    Ajout verification administrateur

--- back/public/AdminController.php
+++ back/public/AdminController.php
@@ -9,22 +9,22 @@ $commentRepository = new \Comment\CommentRepository($pdo);
 
 Router::get('/api/admin/comments', function($request) use($commentHydrator, $commentRepository)
 {
-    // if (!array_key_exists('HTTP_AUTHORIZATION', $_SERVER)) {
-    //     http_response_code(401);
-    //     echo json_encode(['error' => 'You are not authorized without JWT']);
-    //     return;
-    // }
+    if (!array_key_exists('HTTP_AUTHORIZATION', $_SERVER)) {
+        http_response_code(401);
+        echo json_encode(['error' => 'You are not authorized without JWT']);
+        return;
+    }
 
-    // [, $token] = explode(' ', $_SERVER['HTTP_AUTHORIZATION']);
+    [, $token] = explode(' ', $_SERVER['HTTP_AUTHORIZATION']);
 
     try {
-        // $userId = \Token\JwtHS256::validate($token, getenv('SECRET'));
-        // $user = $userRepository->fetchFullById($userId);
-        // if($user->getRole() !== 'ADMIN'){
-        //     echo json_encode(['error' => 'Vous avez besoin de privillèges administrateur pour accèder cette information.']);
-        //     http_response_code(401);
-        //     return;
-        // }
+        $userId = \Token\JwtHS256::validate($token, getenv('SECRET'));
+        $user = $userRepository->fetchFullById($userId);
+        if($user->getRole() !== 'ADMIN'){
+            echo json_encode(['error' => 'Vous avez besoin de privillèges administrateur pour accèder cette information.']);
+            http_response_code(401);
+            return;
+        }
         $comments = $commentRepository->fetchAll();
         if(!$comments) return http_response_code(404);
         echo json_encode($commentHydrator->extractAll($comments), JSON_UNESCAPED_UNICODE);
