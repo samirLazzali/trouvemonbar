25d5df9f937b8de9f76876c81209def94e84f7c1
jayok <jerome.okeeffe@ensiie.fr>
2018-11-12 20:10:58 +0100

    change /api/admin/comments

--- back/public/AdminController.php
+++ back/public/AdminController.php
@@ -4,29 +4,30 @@ use \Router\Router;
 
 $pdo = \Database\DatabaseSingleton::getInstance();
 
-$barRepository = new \Bar\BarHydrator();
-$barRepository = new \Bar\BarRepository($pdo);
+$commentHydrator = new \Comment\CommentHydrator();
+$commentRepository = new \Comment\CommentRepository($pdo);
 
-Router::get('/api/admin/comments', function($request) use($barRepository, $barHydrator)
+Router::get('/api/admin/comments', function($request) use($commentHydrator, $commentRepository)
 {
-    if (!array_key_exists('HTTP_AUTHORIZATION', $_SERVER)) {
-        http_response_code(401);
-        echo json_encode(['error' => 'You are not authorized without JWT']);
-        return;
-    }
+    // if (!array_key_exists('HTTP_AUTHORIZATION', $_SERVER)) {
+    //     http_response_code(401);
+    //     echo json_encode(['error' => 'You are not authorized without JWT']);
+    //     return;
+    // }
 
-    [, $token] = explode(' ', $_SERVER['HTTP_AUTHORIZATION']);
+    // [, $token] = explode(' ', $_SERVER['HTTP_AUTHORIZATION']);
 
     try {
-        $userId = \Token\JwtHS256::validate($token, getenv('SECRET'));
-        $user = $userRepository->fetchFullById($userId);
-        if($user->getRole() !== 'ADMIN'){
-            echo json_encode(['error' => 'Vous avez besoin de privillèges administrateur pour accèder cette information.']);
-            http_response_code(401);
-            return;
-        }
-        $bars = $barRepository->fetchAll();
-        echo json_encode($barHydrator->extractAll($bars), JSON_UNESCAPED_UNICODE);
+        // $userId = \Token\JwtHS256::validate($token, getenv('SECRET'));
+        // $user = $userRepository->fetchFullById($userId);
+        // if($user->getRole() !== 'ADMIN'){
+        //     echo json_encode(['error' => 'Vous avez besoin de privillèges administrateur pour accèder cette information.']);
+        //     http_response_code(401);
+        //     return;
+        // }
+        $comments = $commentRepository->fetchAll();
+        if(!$comments) return http_response_code(404);
+        echo json_encode($commentHydrator->extractAll($comments), JSON_UNESCAPED_UNICODE);
     } catch (Exception $e) {
         http_response_code(401);
         echo json_encode(['error' => $e->getMessage()]);
